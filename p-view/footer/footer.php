b741bde90ecc4ecc871e747585918e96a24aed93
Fukuball Lin <fukuball@gmail.com>
2012-05-17 20:21:45 +0800

    add footer

---
+++ p-view/footer/footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * footer.php is the view of main footer
+ * 
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /p-view/footer/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  No Licence
+ * @version  Release: <1.0>
+ * @link     http://sarasti.cs.nccu.edu.tw
+ */
+ 
+?>
+<footer id="footer">
+   <p>
+   	<a href="<?=SITE_HOST?>" title="首頁">
+   	   首頁
+   	</a> | 
+   	<a href="<?=SITE_HOST?>/lyrics/" title="詞庫">
+   	   詞庫
+   	</a>
+      <br/>
+   	版權所有 ©2012  DM Lab all rights reserved.
+   </p>
+</footer>
