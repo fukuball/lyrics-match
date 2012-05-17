787003c1f5b9a6570f77e0b3d8c4f414e70ba350
Fukuball Lin <fukuball@gmail.com>
2012-05-17 20:00:25 +0800

    add icon meta

---
+++ p-view/head/icon-include.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * icon-include.php is the view of icon link tag
+ * 
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /p-view/head/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  No Licence
+ * @version  Release: <1.0>
+ * @link     http://sarasti.cs.nccu.edu.tw
+ */
+?>
+<link rel="shortcut icon" href="/iv-asset/image/favicon.ico?v1" />
+<link rel="apple-touch-icon" href="/iv-asset/image/index-touch-icon.png?v1" />
+<link rel="apple-touch-icon" sizes="72x72" href="/iv-asset/image/index-touch-icon-72x72.png?v1" />
+<link rel="apple-touch-icon" sizes="114x114" href="/iv-asset/image/index-touch-icon-114x114.png?v1" />
