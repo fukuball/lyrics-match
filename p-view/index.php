02cc5e79744c0cb3a6f6cefad95c440b2b5c6492
Fukuball Lin <fukuball@gmail.com>
2012-05-17 22:24:12 +0800

    update index page

--- p-view/index.php
+++ p-view/index.php
@@ -1,13 +1,6 @@
-<div class="page-header padding-all">
+<div class="page-header padding-all align-center">
    <h1>Lyrics Match - Please Upload Youre Music Work!</h1>
 </div>
-<blockquote class="padding-all">
-   <p>
-      File Upload widget with multiple file selection, drag&amp;drop support, progress bars and preview images for jQuery.<br/>
-      Supports cross-domain, chunked and resumable file uploads and client-side image resizing.<br/>
-      Works with any server-side platform (PHP, Python, Ruby on Rails, Java, Node.js, Go etc.) that supports standard HTML form file uploads.
-   </p>
-</blockquote>
 <br/>
 <div class="well" style="width: 300px; margin: 10px auto;">
    <input id="fileupload" type="file" name="files[]" data-url="<?=SITE_HOST?>/p-library/blueimp/server/php/" multiple />
