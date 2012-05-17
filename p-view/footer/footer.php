6acd831b18903afeec21b12b003480c274b8923c
Fukuball Lin <fukuball@gmail.com>
2012-05-17 20:51:12 +0800

    add header

--- p-view/footer/footer.php
+++ p-view/footer/footer.php
@@ -15,11 +15,11 @@
 ?>
 <footer id="footer">
    <p>
-   	<a href="<?=SITE_HOST?>" title="首頁">
+   	<a href="<?=SITE_HOST?>/" title="首頁">
    	   首頁
    	</a> | 
-   	<a href="<?=SITE_HOST?>/lyrics/" title="詞庫">
-   	   詞庫
+   	<a href="<?=SITE_HOST?>/music/" title="音樂">
+   	   音樂
    	</a>
       <br/>
    	版權所有 ©2012  DM Lab all rights reserved.
---
+++ p-view/header/header.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * header.php is the view of main header
+ * 
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /p-view/header/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  No Licence
+ * @version  Release: <1.0>
+ * @link     http://sarasti.cs.nccu.edu.tw
+ */
+
+$home_header_active = '';
+$music_header_active = '';
+
+switch ($current_page_path_url) {
+   
+case '/lyrics-match':
+case '/lyrics-match/':
+case '/lyrics-match/index.html':
+
+   $home_header_active = ' active';
+
+   break;
+
+case '/lyrics-match/music':
+case (preg_match('/lyrics-match/music\/.*/', $current_page_path_url) ? true : false) :
+
+   $music_header_active = ' active';
+
+   break;
+      
+}
+ 
+?>
+<header id="top-bar" class="navbar navbar-fixed-top">
+   <div class="navbar-inner">
+      <div class="container">
+         <a id="p-collapse-btn" class="btn btn-navbar" title="導覽列">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+         </a>
+         <div id="logo">
+            <a class="brand" href="<?=SITE_HOST?>/" title="Lyrics Match">Lyrics Match</a>
+         </div>
+         <div id="p-nav-collapse" class="nav-collapse">
+            <nav>
+               <ul class="nav">
+                  <li id="header-home-tab" class="main-nav<?php echo $home_header_active; ?>">
+                     <a href="<?=SITE_HOST?>/" title="首頁">
+                        首頁
+                     </a>
+                  </li>
+                  <li id="header-music-tab" class="main-nav<?php echo $music_header_active; ?>">
+                     <a href="<?=SITE_HOST?>/music/" title="音樂">
+                        音樂
+                     </a>
+                  </li>
+               </ul>
+            </nav>
+            <ul class="nav pull-right">
+               <li class="divider-vertical"></li>
+               <li>
+                  <a id="header-login" title="登入">
+                     登入
+                  </a>
+               </li>
+               <li class="divider-vertical"></li>
+            </ul>
+         </div><!--/.nav-collapse -->
+      </div>
+   </div>
+</header>
+<div class="align-center">
+   <span id="system-message"> 處理中 ... </span>
+</div>
+<div id="p-modal-block">
+</div>
