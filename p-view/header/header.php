6a61b4981ebac156db30e4729c933ba8d6c70e11
Fukuball Lin <fukuball@gmail.com>
2012-05-17 21:05:10 +0800

    update style

--- p-view/header/header.php
+++ p-view/header/header.php
@@ -43,7 +43,7 @@ case (preg_match('/music\/.*/', $current_page_path_url) ? true : false) :
             <span class="icon-bar"></span>
          </a>
          <div id="logo">
-            <a class="font-bolder brand" href="<?=SITE_HOST?>/" title="Lyrics Match">Lyrics Match</a>
+            <a class="brand" href="<?=SITE_HOST?>/" title="Lyrics Match">Lyrics Match</a>
          </div>
          <div id="p-nav-collapse" class="nav-collapse">
             <nav>
