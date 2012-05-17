f2385112a00d8e4565ced2a08563a3e9e9ef046b
Fukuball Lin <fukuball@gmail.com>
2012-05-17 20:16:06 +0800

    add style

---
+++ p-view/head/style-include.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * style-include.php is head include style
+ * 
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /p-view/head/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  No Licence
+ * @version  Release: <1.0>
+ * @link     http://sarasti.cs.nccu.edu.tw
+ */
+?>
+<!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+<!--[if lt IE 9]>
+   <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<link href="<?=SITE_HOST?>/p-asset/stylesheet/ui-darkness/jquery-ui-1.8.17.custom.css" type="text/css" rel="stylesheet">
+<link href="<?=SITE_HOST?>/p-asset/stylesheet/prettify.css" type="text/css" rel="stylesheet" />
+<link href="<?=SITE_HOST?>/p-asset/stylesheet/bootstrap/bootstrap.css" type="text/css" rel="stylesheet">
+<link href="<?=SITE_HOST?>/p-asset/stylesheet/index.css" type="text/css" rel="stylesheet">
