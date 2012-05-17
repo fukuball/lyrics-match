f8ffbb2e952127aa2855105b74ea1f0335bfc2ae
Fukuball Lin <fukuball@gmail.com>
2012-05-17 22:40:48 +0800

    update index page

--- p-view/index.php
+++ p-view/index.php
@@ -1,12 +1,13 @@
 <div class="page-header padding-all align-center">
    <h1>Lyrics Match - Please Upload Your Music Work!</h1>
 </div>
-<br/>
-<div style="width: 800px; margin: 20px auto;">
-   <img width="800" src="<?=SITE_HOST?>/p-asset/image/ui-icon/banner.png" />
-</div>
+<br class="clearboth" />
 <div class="well" style="width: 300px; margin: 10px auto;">
    <input id="fileupload" type="file" name="files[]" data-url="<?=SITE_HOST?>/p-library/blueimp/server/php/" multiple />
+</div>
+<br class="clearboth" />
+<div style="width: 800px; margin: 20px auto;">
+   <img width="800" src="<?=SITE_HOST?>/p-asset/image/ui-icon/banner.png" />
 </div>
 <br class="clearboth" />
 <script>
