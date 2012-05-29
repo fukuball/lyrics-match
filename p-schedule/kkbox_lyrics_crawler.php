94ae6a41a4d8eb2fc23eb6ff8185cc2908c813b1
Fukuball Lin <fukuball@gmail.com>
2012-05-29 19:39:21 +0800

    debugging

--- p-schedule/kkbox_lyrics_crawler.php
+++ p-schedule/kkbox_lyrics_crawler.php
@@ -30,6 +30,9 @@ foreach ($query_result as $query_result_data) {
    $song_title = $query_result_data['song_title'];
    $midi_path = $query_result_data['midi_path'];
 
+   $search_resp_html_dom = file_get_html("http://tw.kkbox.com/search.php?word=".urlencode($song_title)."&search=song&search_lang=");
+   print_r($search_resp_html_dom);
+
    /*$search_resp = LMHelper::doGet("http://tw.kkbox.com/search.php?word=".urlencode($song_title)."&search=song&search_lang=");
    $search_resp_html_dom = str_get_html($search_resp);
 
