95aad8a3685c3b130c5cd9e611e29abd12f60edd
Fukuball Lin <fukuball@gmail.com>
2012-05-29 17:28:30 +0800

    working

--- p-play-ground/fukuball/scan_directory.php
+++ p-play-ground/fukuball/scan_directory.php
@@ -2,6 +2,9 @@
 
 require_once dirname(dirname(dirname(__FILE__)))."/p-config/application-setter.php";
 
+$db_obj = LMDBAccess::init();
+
+
 $female_dir = SITE_ROOT."/p-data/midi/female";
 $dir_handler  = opendir($female_dir);
 while (false !== ($filename = readdir($dir_handler))) {
@@ -27,7 +30,16 @@ foreach ($files as $key => $subdirectory ) {
 
          if ($midi_file != '..' && $midi_file != '.') {
 
-            echo $female_dir.'/'.$subdirectory.'/'.$midi_file."\n";
+            $midi_file_name_array = explode('.', $midi_file);
+            $midi_path = $female_dir.'/'.$subdirectory.'/'.$midi_file;
+            $artist_title = $subdirectory;
+            $song_title = $midi_file_name_array[0];
+
+            echo $artist_title."\n";
+            echo $song_title."\n";
+            echo $midi_path."\n";
+
+            //$db_obj->selectCommand($select_sql);
 
          }
 
