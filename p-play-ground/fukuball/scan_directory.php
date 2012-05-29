0ce07a1b5d68e28a6284e8ab18083563f787d5d8
Fukuball Lin <fukuball@gmail.com>
2012-05-29 17:56:25 +0800

    debugging

--- p-play-ground/fukuball/scan_directory.php
+++ p-play-ground/fukuball/scan_directory.php
@@ -22,75 +22,87 @@ foreach ($files as $key => $subdirectory ) {
 
    if ($subdirectory != '..' && $subdirectory != '.') {
 
-      $innerdir_handler  = opendir($female_dir.'/'.$subdirectory);
-      while (false !== ($midifilename = readdir($innerdir_handler))) {
-
-         $midifiles[] = $midifilename;
-
-      }
-      sort($midifiles);
-
-      foreach ($midifiles as $key => $midi_file ) {
-
-         if ($midi_file != '..' && $midi_file != '.') {
-
-            $count++;
-            $midi_file_name_array = explode('.', $midi_file);
-            $midi_path = $female_dir.'/'.$subdirectory.'/'.$midi_file;
-            $artist_title = $subdirectory;
-            $song_title = $midi_file_name_array[0];
-
-            echo $artist_title."\n";
-            echo $song_title."\n";
-            echo $midi_path."\n";
-            echo $count."\n";
-
-            /*$select_sql = "SELECT ".
-                          "id ".
-                          "FROM temp_midi ".
-                          "WHERE ".
-                          "artist_title='$artist_title' ".
-                          "AND song_title='$song_title'";
-
-            $query_result = $db_obj->selectCommand($select_sql);
-            foreach ($query_result as $query_result_data) {
-
-               $temp_midi_id = $query_result_data['id'];
-
-            }
-            if (empty($temp_midi_id)) {
-
-               $insert_sql = "INSERT ".
-                             "INTO temp_midi ".
-                             "(".
-                             "artist_title, ".
-                             "song_title, ".
-                             "midi_path ".
-                             ") ".
-                             "VALUES ".
-                             "(".
-                             "'$artist_title', '$song_title', '$midi_path'".
-                             ")";
-
-               if ($db_obj->insertCommand($insert_sql)) {
-                  echo $artist_title."\n";
-                  echo $song_title."\n";
-                  echo $midi_path."\n";
-                  echo $insert_sql."\n";
-               } else {
-                  echo "fail \n";
-               }
-
-            }*/
-
-
-         }
-
-      }
+      $count++;
+      echo $subdirectory."\n";
+      echo $count."\n";
 
    }
 
 }
+
+//foreach ($files as $key => $subdirectory ) {
+//
+//   if ($subdirectory != '..' && $subdirectory != '.') {
+//
+//      $innerdir_handler  = opendir($female_dir.'/'.$subdirectory);
+//      while (false !== ($midifilename = readdir($innerdir_handler))) {
+//
+//         $midifiles[] = $midifilename;
+//
+//      }
+//      sort($midifiles);
+//
+//      foreach ($midifiles as $key => $midi_file ) {
+//
+//         if ($midi_file != '..' && $midi_file != '.') {
+//
+//            $count++;
+//            $midi_file_name_array = explode('.', $midi_file);
+//            $midi_path = $female_dir.'/'.$subdirectory.'/'.$midi_file;
+//            $artist_title = $subdirectory;
+//            $song_title = $midi_file_name_array[0];
+//
+//            echo $artist_title."\n";
+//            echo $song_title."\n";
+//            echo $midi_path."\n";
+//            echo $count."\n";
+//
+//            /*$select_sql = "SELECT ".
+//                          "id ".
+//                          "FROM temp_midi ".
+//                          "WHERE ".
+//                          "artist_title='$artist_title' ".
+//                          "AND song_title='$song_title'";
+//
+//            $query_result = $db_obj->selectCommand($select_sql);
+//            foreach ($query_result as $query_result_data) {
+//
+//               $temp_midi_id = $query_result_data['id'];
+//
+//            }
+//            if (empty($temp_midi_id)) {
+//
+//               $insert_sql = "INSERT ".
+//                             "INTO temp_midi ".
+//                             "(".
+//                             "artist_title, ".
+//                             "song_title, ".
+//                             "midi_path ".
+//                             ") ".
+//                             "VALUES ".
+//                             "(".
+//                             "'$artist_title', '$song_title', '$midi_path'".
+//                             ")";
+//
+//               if ($db_obj->insertCommand($insert_sql)) {
+//                  echo $artist_title."\n";
+//                  echo $song_title."\n";
+//                  echo $midi_path."\n";
+//                  echo $insert_sql."\n";
+//               } else {
+//                  echo "fail \n";
+//               }
+//
+//            }*/
+//
+//
+//         }
+//
+//      }
+//
+//   }
+//
+//}
 
 
 require_once SITE_ROOT."/p-config/application-unsetter.php";
