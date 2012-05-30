996ec657a423bf01a94907e727300ca4391c532e
Fukuball Lin <fukuball@gmail.com>
2012-05-31 03:46:03 +0800

    working

--- p-schedule/kkbox_lyrics_crawler.php
+++ p-schedule/kkbox_lyrics_crawler.php
@@ -90,7 +90,7 @@ foreach ($query_result as $query_result_data) {
          $in_disc_name = trim($song_page_dom->query->results->body->div[3]->div[0]->ul->li[2]->a->content);
          $in_disc_url = $kkbox_link.$song_page_dom->query->results->body->div[3]->div[0]->ul->li[2]->a->href;
          $in_disc_src = $song_page_dom->query->results->body->div[3]->div[1]->div[0]->div[0]->div->div[0]->img->src;
-         $kk_disc_info = $song_page_dom->query->results->body->div[3]->div[1]->div[0]->div[0]->div->div[1];
+         $kk_disc_info = $song_page_dom->query->results->body->div[3]->div[1]->div[0]->div[0]->div->div[1]->dl->dd;
 
          print_r($kk_disc_info);
 
