6ab250c436e0084962387d0d5610805f3fbd87ce
Fukuball Lin <fukuball@gmail.com>
2012-05-30 19:03:38 +0800

    working

--- p-class/helper/LMHelper.php
+++ p-class/helper/LMHelper.php
@@ -180,6 +180,8 @@ class LMHelper
       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
       curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:12.0) Gecko/20100101 Firefox/12.0');
       curl_setopt($ch, CURLOPT_COOKIE, 'BIGipServerTWWSs_main=855703562.20480.0000; __utma=129119753.1274697662.1338372832.1338372832.1338372832.1; __utmb=129119753.3.10.1338372832; __utmc=129119753; __utmz=129119753.1338372832.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __atuvc=3|22');
+      curl_setopt($ch, CURLOPT_COOKIEJAR, "cookie.txt");
+      curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie.txt");
 
       $response = curl_exec($ch);
       curl_close($ch);
