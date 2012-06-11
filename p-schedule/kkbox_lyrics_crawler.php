c53cffd055a121ac66b3a65a138866d24439e3f7
Fukuball Lin <fukuball@gmail.com>
2012-06-11 16:51:45 +0800

    update

--- p-schedule/kkbox_lyrics_crawler.php
+++ p-schedule/kkbox_lyrics_crawler.php
@@ -27,7 +27,7 @@ $select_sql = "SELECT ".
               "* ".
               "FROM temp_midi ".
               "WHERE ".
-              "is_moved='0' AND id>'1383' ";
+              "is_moved='0' AND id>'1404' ";
 
 $query_result = $db_obj->selectCommand($select_sql);
 
