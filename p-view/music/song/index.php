b39f7c6dedb8e614a0f7fabbf2e1357f3dbc7e5d
Fukuball Lin <fukuball@gmail.com>
2012-06-18 17:30:22 +0800

    update song page

--- p-view/music/song/index.php
+++ p-view/music/song/index.php
@@ -11,6 +11,7 @@
  * @version  Release: <1.0>
  * @link     http://sarasti.cs.nccu.edu.tw
  */
+if (!empty($_GET['song_id'])) {
 ?>
 <ul class="breadcrumb">
    <li>
@@ -20,6 +21,23 @@
       歌曲資料
    </li>
 </ul>
+<?php
+   $song_obj = new IndievoxSong($_GET['song_id']);
+   $disc_obj = new IndievoxDisc($song_obj->disc_id);
+?>
 <div id='song-item-block'>
-
+   <div class="row">
+      <div width="300px">
+         <a href="#" class="thumbnail">
+            <img src="<?=$disc_obj->cover_path?>" alt="cover">
+         </a>
+      </div>
+      <div width="700px">
+      </div>
+   </div>
 </div>
+<?php
+   unset($song_obj);
+   unset($disc_obj);
+}
+?>
