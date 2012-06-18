ee9363818eddbdf34cbc9ae55a592743b413f4af
Fukuball Lin <fukuball@gmail.com>
2012-06-18 17:37:07 +0800

    update song page

--- p-view/music/song/index.php
+++ p-view/music/song/index.php
@@ -24,15 +24,19 @@ if (!empty($_GET['song_id'])) {
 <?php
    $song_obj = new LMSong($_GET['song_id']);
    $disc_obj = new LMDisc($song_obj->disc_id);
+   $performer_obj = new LMPerformer($song_obj->performer_id);
 ?>
 <div id='song-item-block'>
    <div class="row">
-      <div width="300px">
+      <div style="width:300px;">
          <a href="#" class="thumbnail">
             <img src="<?=$disc_obj->cover_path?>" alt="cover">
          </a>
       </div>
-      <div width="700px">
+      <div style="width:700px;">
+         <h2><?=$song_obj->title?></h2>
+         <h3><?=$performer_obj->name?></h3>
+         <h3><?=$disc_obj->title?></h3>
       </div>
    </div>
 </div>
