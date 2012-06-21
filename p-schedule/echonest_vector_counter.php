1e8f2582e1e3401e8c8c9552781c08f3af6ea03c
Fukuball Lin <fukuball@gmail.com>
2012-06-21 16:49:00 +0800

    add schedule

---
+++ p-schedule/echonest_vector_counter.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * echonest_vector_counter.php to count feature vector
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /p-schedule/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  No Licence
+ * @version  Release: <1.0>
+ * @link     http://sarasti.cs.nccu.edu.tw
+ */
+
+require_once dirname(dirname(__FILE__))."/p-config/application-setter.php";
+
+
+$db_obj = LMDBAccess::getInstance();
+
+$select_sql = "SELECT ".
+              "id ".
+              "FROM song ".
+              "WHERE is_deleted = '0' ".
+              "AND audio_path!='' ".
+              "AND echonest_track_id!='' ".
+              "AND retrieval_status='success' ".
+              "ORDER BY id DESC ";
+
+$query_result = $db_obj->selectCommand($select_sql);
+
+// get unprocess data
+foreach ($query_result as $query_result_data) {
+
+   $song_obj = new LMSong($query_result_data['id']);
+   $echonest_analysis_file = AUDIO_ROOT.'/'.$song_obj->getId().'.json';
+
+}
+
+require_once SITE_ROOT."/p-config/application-unsetter.php";
+
+?>
