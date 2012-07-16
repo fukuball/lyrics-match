21fe6fee2ac394f9742e236f095fffaad9d80ed3
Fukuball Lin <fukuball@gmail.com>
2012-07-16 22:08:30 +0800

    debugging

--- p-library/model/music_feature/genrenate_music_feature_model.py
+++ p-library/model/music_feature/genrenate_music_feature_model.py
@@ -64,10 +64,14 @@ A_bar_music_feature_matrix = np.dot(music_feature_U,np.dot(music_feature_s,music
 
 A_bar_string = A_bar_music_feature_matrix.tostring()
 
-try:
-   cur.execute("""INSERT INTO music_feature_matrix (matrix,row_song_id,column_music_feature,augment_music_feature,augment_matrix,type,create_time,modify_time) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""",(A_bar_string, row_song_id, column_music_feature, augment_music_feature, augment_matrix, "model", create_time, modify_time))
-   db.commit()
-   print "success"
-except mysql.Error, e:
-   db.rollback()
-   print "An error has been passed. %s" %e
+A_bar_string2 = ""
+for A_bar_row in A_bar_music_feature_matrix:
+   print A_bar_row
+
+#try:
+#   cur.execute("""INSERT INTO music_feature_matrix (matrix,row_song_id,column_music_feature,augment_music_feature,augment_matrix,type,create_time,modify_time) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""",(A_bar_string, row_song_id, column_music_feature, augment_music_feature, augment_matrix, "model", create_time, modify_time))
+#   db.commit()
+#   print "success"
+#except mysql.Error, e:
+#   db.rollback()
+#   print "An error has been passed. %s" %e
