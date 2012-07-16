4f84de96f99c6a17903a89409e96b7edec42fac4
Fukuball Lin <fukuball@gmail.com>
2012-07-16 17:55:13 +0800

    debugging

--- p-library/model/music_feature/genrenate_music_feature_model.py
+++ p-library/model/music_feature/genrenate_music_feature_model.py
@@ -45,6 +45,7 @@ for s_index, s_item in enumerate(music_feature_s) :
    if (s_item<10) :
       music_feature_s[s_index] = 0.0
 
-A_bar_music_feature_matrix = np.dot(music_feature_U,music_feature_s,music_feature_V)
+temp_music_feature_matrix = np.dot(music_feature_U,music_feature_s)
+A_bar_music_feature_matrix = np.dot(temp_music_feature_matrix,music_feature_V)
 
 print A_bar_music_feature_matrix
