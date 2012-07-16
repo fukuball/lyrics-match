777c30f75458f2d6c6b613df7593006befca2879
Fukuball Lin <fukuball@gmail.com>
2012-07-16 16:35:23 +0800

    add matrix

---
+++ p-library/model/ImportPath.py
@@ -0,0 +1,17 @@
+#
+# ImportPath.py to import lib path
+#
+# Python version 2.6.8
+#
+# @category Python
+# @package  /p-library/model/
+# @author   Fukuball Lin <fukuball@gmail.com>
+# @license  No Licence
+# @version  Release: <1.0>
+# @link     http://sarasti.cs.nccu.edu.tw
+#
+# hard code
+
+def Import():
+   import sys
+   sys.path.append("/var/www/html/lyrics-match/p-config")
--- p-library/model/music_feature/genrenate_music_feature_model.py
+++ p-library/model/music_feature/genrenate_music_feature_model.py
@@ -1,11 +1,56 @@
 #!/usr/bin/python26
 # -*- coding:utf-8 -*-
 
+import sys
 import numpy as np
+import MySQLdb as mysql
 
-A = np.floor(np.random.rand(4,4)*20-10) # generating a random
-b = np.floor(np.random.rand(4,1)*20-10) # system Ax=b
+sys.path.append("/var/www/html/lyrics-match/p-config")
 
-U,s,V = np.linalg.svd(A) # SVD decomposition of A
+CONST = _Const()
 
-print U
+print CONST.DBHOST
+
+#def Import():
+#  import sys
+#  sys.path.append("C:/Users/18Fan/Dropbox/研究/程式實作/Lyrics_Process/Lyrics_Form_Analysis".decode('utf-8'))
+#  sys.path.append("C:/Users/18Fan/Dropbox/研究/程式實作/Lyrics_Process/Network_Service".decode('utf-8'))
+#
+#
+#  """
+#  加入模組檔案路徑
+#  sys.path.append(檔案路徑)
+#  """
+#
+#
+#
+#
+#"""
+#在其他檔案加入模組範例
+#"""
+#import ImportPath
+#ImportPath.Import()
+
+#db = mysql.connect(host="localhost",   # your host, usually localhost
+#                   user="john",        # your username
+#                   passwd="megajonhy", # your password
+#                   db="jonhydb")       # name of the data base
+#
+## you must create a Cursor object. It will let
+##  you execute all the query you need
+#cur = db.cursor()
+#
+## Use all the SQL you like
+#cur.execute("SELECT * FROM YOUR_TABLE_NAME")
+#
+## print all the first cell of all the rows
+#for row in cur.fetchall() :
+#    print row[0]
+#
+#
+#A = np.floor(np.random.rand(4,4)*20-10) # generating a random
+#b = np.floor(np.random.rand(4,1)*20-10) # system Ax=b
+#
+#U,s,V = np.linalg.svd(A) # SVD decomposition of A
+#
+#print U
