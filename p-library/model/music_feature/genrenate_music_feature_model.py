8c782d644c284e8f80adfad1ad572139779c7a5f
Fukuball Lin <fukuball@gmail.com>
2012-07-16 16:46:20 +0800

    add matrix

--- p-library/model/music_feature/genrenate_music_feature_model.py
+++ p-library/model/music_feature/genrenate_music_feature_model.py
@@ -5,48 +5,25 @@ import sys
 import numpy as np
 import MySQLdb as mysql
 
-sys.path.append("/var/www/html/lyrics-match/p-config")
-import db_stage
+sys.path.append("/var/www/html/lyrics-match/p-library/model")
+import ImportPath
+ImportPath.Import()
 
+import db_stage
 CONST = db_stage._Const()
 
-print CONST.DBHOST
 
-#def Import():
-#  import sys
-#  sys.path.append("C:/Users/18Fan/Dropbox/研究/程式實作/Lyrics_Process/Lyrics_Form_Analysis".decode('utf-8'))
-#  sys.path.append("C:/Users/18Fan/Dropbox/研究/程式實作/Lyrics_Process/Network_Service".decode('utf-8'))
-#
-#
-#  """
-#  加入模組檔案路徑
-#  sys.path.append(檔案路徑)
-#  """
-#
-#
-#
-#
-#"""
-#在其他檔案加入模組範例
-#"""
-#import ImportPath
-#ImportPath.Import()
+# connect to db
+db = mysql.connect(host    = CONST.DBHOST,
+                   user    = CONST.DBUSER,
+                   passwd  = CONST.DBPASS,
+                   db      = CONST.DBNAME)
 
-#db = mysql.connect(host="localhost",   # your host, usually localhost
-#                   user="john",        # your username
-#                   passwd="megajonhy", # your password
-#                   db="jonhydb")       # name of the data base
-#
-## you must create a Cursor object. It will let
-##  you execute all the query you need
-#cur = db.cursor()
-#
-## Use all the SQL you like
-#cur.execute("SELECT * FROM YOUR_TABLE_NAME")
-#
-## print all the first cell of all the rows
-#for row in cur.fetchall() :
-#    print row[0]
+cur = db.cursor()
+cur.execute("SELECT * FROM music_feature_matrix WHERE id=1")
+
+for row in cur.fetchall() :
+   print row['matrix']
 #
 #
 #A = np.floor(np.random.rand(4,4)*20-10) # generating a random
