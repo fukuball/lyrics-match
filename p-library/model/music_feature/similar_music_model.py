61e01f7fd5f252017df2e6850c19593be1f2aa56
Fukuball Lin <fukuball@gmail.com>
2012-07-17 01:58:13 +0800

    debugging

--- p-library/model/music_feature/similar_music_model.py
+++ p-library/model/music_feature/similar_music_model.py
@@ -112,7 +112,7 @@ if (has_feature_data=="true" and has_model_data=="true") :
 
    similar_song_string = ""
    for similar_song_id in similar_music_sort_dic[:30]:
-      similar_song_string += similar_song_id+":"+similar_music_sort_dic[int(similar_song_id)]+","
+      similar_song_string += similar_song_id+":"+similar_music_dic[int(similar_song_id)]+","
 
    print similar_song_string
 
