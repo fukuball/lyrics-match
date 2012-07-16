3746a601673664abdc515e047f690bfae44a710f
Fukuball Lin <fukuball@gmail.com>
2012-07-16 17:20:31 +0800

    debugging

--- p-library/model/music_feature/genrenate_music_feature_model.py
+++ p-library/model/music_feature/genrenate_music_feature_model.py
@@ -50,9 +50,9 @@ print( "shape --> %d rows x %d columns" % V.shape )
 
 #
 #
-#A = np.floor(np.random.rand(4,4)*20-10) # generating a random
-#b = np.floor(np.random.rand(4,1)*20-10) # system Ax=b
-#
-#U,s,V = np.linalg.svd(A) # SVD decomposition of A
-#
-#print U
+A = np.floor(np.random.rand(4,4)*20-10) # generating a random
+b = np.floor(np.random.rand(4,1)*20-10) # system Ax=b
+
+U,s,V = np.linalg.svd(A) # SVD decomposition of A
+
+print s
