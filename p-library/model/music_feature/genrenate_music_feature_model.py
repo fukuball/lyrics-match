c86919222f39a408b0828c0bd5f89213c85682dc
Fukuball Lin <fukuball@gmail.com>
2012-07-16 17:53:53 +0800

    debugging

--- p-library/model/music_feature/genrenate_music_feature_model.py
+++ p-library/model/music_feature/genrenate_music_feature_model.py
@@ -37,12 +37,14 @@ A_music_feature_matrix = np.matrix(music_feature_matrix)
 # print A_music_feature_matrix
 # print( "shape --> %d rows x %d columns" % A_music_feature_matrix.shape )
 
-music_feature_U,music_feature_s,music_feature_V = np.linalg.svd(A_music_feature_matrix) # SVD decomposition
+# SVD decomposition
+music_feature_U,music_feature_s,music_feature_V = np.linalg.svd(A_music_feature_matrix)
 
-print music_feature_s
-
+# 降維
 for s_index, s_item in enumerate(music_feature_s) :
    if (s_item<10) :
       music_feature_s[s_index] = 0.0
 
-print music_feature_s
+A_bar_music_feature_matrix = np.dot(music_feature_U,music_feature_s,music_feature_V)
+
+print A_bar_music_feature_matrix
