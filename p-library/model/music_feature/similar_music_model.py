9a9889ce55ea5f54c5fc56e876a0018e057288ee
Fukuball Lin <fukuball@gmail.com>
2012-07-16 23:31:40 +0800

    debugging

--- p-library/model/music_feature/similar_music_model.py
+++ p-library/model/music_feature/similar_music_model.py
@@ -33,6 +33,7 @@ db = mysql.connect(host    = CONST.DBHOST,
                    db      = CONST.DBNAME)
 
 song_id = sys.argv[1];
+model_id = sys.argv[2];
 
 cur = db.cursor()
 cur.execute("""SELECT s.mode, s.tempo, s.time_signature, s.energy, s.danceability, s.speechiness, s.loudness, mf.bar_count, mf.beat_count, mf.tatum_count, mf.section_count, mf.segment_count, mf.bar_avg_second, mf.beat_avg_second, mf.tatum_avg_second, mf.section_avg_second, mf.segment_avg_second, mf.pitch_avg_vector, mf.timbre_avg_vector, mf.pitch_std_vector, mf.timbre_std_vector FROM music_feature mf INNER JOIN song s ON (mf.song_id = s.id) WHERE mf.is_deleted = '0' AND s.is_deleted ='0' AND mf.song_id=%s ORDER BY mf.id""", (song_id))
@@ -66,6 +67,24 @@ for row in cur.fetchall() :
    song_music_feature_str += row[20].replace(","," ")
    has_feature_data = "true"
 
-if (has_feature_data=="true") :
+cur.execute("""SELECT * FROM music_feature_matrix WHERE id=%s""", (model_id))
+
+has_model_data = "false"
+music_feature_matrix = "";
+row_song_id = "";
+column_music_feature = "";
+augment_music_feature = "";
+augment_matrix = "";
+
+for row in cur.fetchall() :
+   music_feature_matrix = row[1];
+   row_song_id = row[2];
+   column_music_feature = row[3];
+   augment_music_feature = row[4];
+   augment_matrix = row[5];
+   has_model_data = "true"
+
+if (has_feature_data=="true" && has_model_data=="true") :
    input_song_matrix = np.matrix(song_music_feature_str)
-   print input_song_matrix
+   music_feature_matrix = np.matrix(music_feature_matrix)
+   print music_feature_matrix
