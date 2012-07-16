5350b2fde20b408a050f910a0a7a4e0b5175afa1
Fukuball Lin <fukuball@gmail.com>
2012-07-17 03:13:06 +0800

    debugging

--- p-library/model/music_feature/similar_music_model.py
+++ p-library/model/music_feature/similar_music_model.py
@@ -99,11 +99,12 @@ if (has_feature_data=="true" and has_model_data=="true") :
    #print similar_music_model
    #print( "matrix shape --> %d rows x %d columns" % similar_music_model.shape )
 
-   #input_song_matrix_normalized = (input_song_matrix.getA() - similar_music_model.getA().min(axis=0)) / similar_music_model.getA().ptp(axis=0)
+   input_song_matrix_normalized = (input_song_matrix.getA() - similar_music_model.getA().min(axis=0)) / similar_music_model.getA().ptp(axis=0)
    #print input_song_matrix_normalized;
-   #similar_music_model_normalized = (similar_music_model.getA() - similar_music_model.getA().min(axis=0)) / similar_music_model.getA().ptp(axis=0)
+   similar_music_model_normalized = (similar_music_model.getA() - similar_music_model.getA().min(axis=0)) / similar_music_model.getA().ptp(axis=0)
    #print similar_music_model_normalized;
-
+   dtest = np.dot(similar_music_model_normalized, np.transpose(input_song_matrix_normalized));
+   print dtest;
 
    dist = (similar_music_model.getA() - input_song_matrix.getA())**2
    dist = np.sum(dist, axis=1)
