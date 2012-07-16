f3d15a45390dd9babe207578d33ba84170e1f812
Fukuball Lin <fukuball@gmail.com>
2012-07-16 15:29:44 +0800

    add matrix

--- p-library/model/music_feature/genrenate_music_feature_model.py
+++ p-library/model/music_feature/genrenate_music_feature_model.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python26
 # -*- coding:utf-8 -*-
 
-print("Hello World!")
+import numpy as np
 
-text = "Hello World Text"
-print(text)
+A = np.floor(np.random.rand(4,4)*20-10) # generating a random
+b = np.floor(np.random.rand(4,1)*20-10) # system Ax=b
+
+U,s,V = np.linalg.svd(A) # SVD decomposition of A
+
+print U
