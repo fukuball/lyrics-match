7db6360fcbc3dd8054986909a513e6069c9acb4a
Fukuball Lin <fukuball@gmail.com>
2012-07-16 18:38:30 +0800

    debugging

--- p-library/model/music_feature/genrenate_music_feature_model.py
+++ p-library/model/music_feature/genrenate_music_feature_model.py
@@ -12,18 +12,13 @@ ImportPath.Import()
 import db_stage
 CONST = db_stage._Const()
 
-#A = np.array([[2,0,0,0],[0,0,0,0]])
-#U,s,V=np.linalg.svd(A,full_matrices=False)
-#S=np.diag(s)
-#print np.dot(U,np.dot(S,V))
-
-
 # connect to db
 db = mysql.connect(host    = CONST.DBHOST,
                    user    = CONST.DBUSER,
                    passwd  = CONST.DBPASS,
                    db      = CONST.DBNAME)
 
+# 從資料庫抓資料
 cur = db.cursor()
 cur.execute("SELECT * FROM music_feature_matrix WHERE id=1")
 
@@ -32,13 +27,14 @@ row_song_id = "";
 column_music_feature = "";
 
 for row in cur.fetchall() :
-
    music_feature_matrix = row[1];
    row_song_id = row[2];
    column_music_feature = row[3];
 
+# music feature matrix
 A_music_feature_matrix = np.matrix(music_feature_matrix)
-print( "shape --> %d rows x %d columns" % A_music_feature_matrix.shape )
+print A_music_feature_matrix
+print( "matrix shape --> %d rows x %d columns" % A_music_feature_matrix.shape )
 
 # SVD decomposition
 music_feature_U,music_feature_s,music_feature_V = np.linalg.svd(A_music_feature_matrix, full_matrices=False)
@@ -48,9 +44,8 @@ for s_index, s_item in enumerate(music_feature_s) :
    if (s_item<10) :
       music_feature_s[s_index] = 0.0
 
+# music feature model matrix
 music_feature_s = np.diag(music_feature_s)
-
 A_bar_music_feature_matrix = np.dot(music_feature_U,np.dot(music_feature_s,music_feature_V))
-
 print A_bar_music_feature_matrix
-print( "shape --> %d rows x %d columns" % A_bar_music_feature_matrix.shape )
+print( "matrix shape --> %d rows x %d columns" % A_bar_music_feature_matrix.shape )
