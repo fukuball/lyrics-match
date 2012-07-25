65544955f191554b5ddcc2fe33a99ca6d0630582
Stuart Fan <18fanfan@gmail.com>
2012-07-25 17:29:53 +0800

    sarasti import path

--- p-play-ground/fanfan/LyricsFormAnalysis/ImportPath.py
+++ p-play-ground/fanfan/LyricsFormAnalysis/ImportPath.py
@@ -2,6 +2,6 @@
 
 def Import():
 	import sys
-	sys.path.append("/home/fanfan/www/html/lyrcs-match/p-play-ground/fanfan/Lib//Lib/Lyrics_Form_Analysis".decode('utf-8'))
-	sys.path.append("/home/fanfan/www/html/lyrcs-match/p-play-ground/fanfan/Lib/Network_Service".decode('utf-8'))
+	sys.path.append("/home/fanfan/www/html/lyrcs-match/p-play-ground/fanfan/Lib/Lyrics_Form_Analysis")
+	sys.path.append("/home/fanfan/www/html/lyrcs-match/p-play-ground/fanfan/Lib/Network_Service")
 
