75cdc25ff9aa16d4a9a464238619d5033a658783
Stuart Fan <18fanfan@gmail.com>
2012-07-25 17:37:24 +0800

    change path for sarasti fanfan play ground

--- p-play-ground/fanfan/LyricsFormAnalysis/ImportPath.py
+++ p-play-ground/fanfan/LyricsFormAnalysis/ImportPath.py
@@ -2,6 +2,6 @@
 
 def Import():
 	import sys
-	sys.path.append("/var/www/html/lyrcs-match/p-play-ground/fanfan/Lib/Lyrics_Form_Analysis")
-	sys.path.append("/var/www/html/lyrcs-match/p-play-ground/fanfan/Lib/Network_Service")
+	sys.path.append("/var/www/html/lyrics-match/p-play-ground/fanfan/Lib/Lyrics_Form_Analysis")
+	sys.path.append("/var/www/html/lyrics-match/p-play-ground/fanfan/Lib/Network_Service")
 
