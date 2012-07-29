b196cef5068419c3a41c19735005d7c7d9e961a5
Fukuball Lin <fukuball@gmail.com>
2012-07-30 00:27:54 +0800

    debugging

--- p-view/music/add-song.php
+++ p-view/music/add-song.php
@@ -61,6 +61,7 @@ $('#check-add-song-form').ready(function() {
             success: function( html_block ) {
                $('#system-message').html('完成');
                $('#system-message').fadeOut();
+               alert(html_block);
                if (html_block=="song_exist") {
                   $('#song-exist-warnig').removeClass('hide');
                } else {
