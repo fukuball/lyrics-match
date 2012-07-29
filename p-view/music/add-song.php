671748672f8d2013e3376395fd6f7053d8a51f8c
Fukuball Lin <fukuball@gmail.com>
2012-07-29 23:59:46 +0800

    add song

--- p-view/music/add-song.php
+++ p-view/music/add-song.php
@@ -12,11 +12,66 @@
  * @link     http://sarasti.cs.nccu.edu.tw
  */
 ?>
-<form name="check_add_song_form" class="well form-inline">
+<form id="check-add-song-form" name="check_add_song_form" class="well form-inline">
    <input id="check-song-title" name="check_song_title" type="text" class="input-medium" placeholder="請輸入歌名">
    <input id="check-artist-name" name="check_artist_name" type="text" class="input-medium" placeholder="請輸入歌手">
    <button id="check-add-song-btn" type="button" class="btn">
       新增歌曲
    </button>
 </form>
+<div id="input-invalid-warnig" class="alert hide">
+  <!--<button class="close" data-dismiss="alert">×</button>-->
+  <strong>Warning!</strong> 輸入資料不可為空
+</div>
+<div id="song-exist-warnig" class="alert hide">
+  <!--<button class="close" data-dismiss="alert">×</button>-->
+  <strong>Warning!</strong> 此歌曲已存在，請增加其他歌曲
+</div>
+<script>
+$('#check-add-song-form').ready(function() {
+
+   $('button#check-add-song-btn').click(function(){
+
+      var is_validated = true;
+      if(!$('#check-song-title').val()){
+         $('#input-invalid-warnig').removeClass('hide');
+         is_validated = false;
+      } else {
+         $('#input-invalid-warnig').removeClass('hide');
+         $('#input-invalid-warnig').addClass('hide');
+      }
+      if(!$('#check-artist-name').val()){
+         $('#input-invalid-warnig').removeClass('hide');
+         is_validated = false;
+      } else {
+         $('#input-invalid-warnig').removeClass('hide');
+         $('#input-invalid-warnig').addClass('hide');
+      }
+
+      if(is_validated){
+         $('#check-add-song-btn').attr("disabled", "disabled");
+         $.ajax({
+            url: '<?=SITE_HOST?>/ajax-action/song-action/check-add-song',
+            type: "POST",
+            data: {check_song_title: $('#check-song-title').val(), check_artist_name: $('#check-artist-name').val()},
+            dataType: "html",
+            beforeSend: function( xhr ) {
+               $('#system-message').html('處理中');
+               $('#system-message').show();
+            },
+            success: function( html_block ) {
+               $('#add-song-form').append(html_block);
+               $('#system-message').html('完成');
+               $('#system-message').fadeOut();
+            }
+         });
+      }
+
+   });
+
+
+});
+</script>
 <hr />
+<div id="add-song-form">
+</div>
