2f65760bbfc9c3e49095351e750b8c42e27f3b45
Fukuball Lin <fukuball@gmail.com>
2012-07-30 17:15:00 +0800

    debugging

--- ajax-action/SongActionView/add-song-form.php
+++ ajax-action/SongActionView/add-song-form.php
@@ -230,7 +230,6 @@
             $('#system-message').html('完成');
             $('#system-message').fadeOut();
             $('#add-song-form').html('');
-            alert("success");
          } else {
             $('#system-message').html('失敗，請重新操作');
             $('#system-message').fadeOut();
