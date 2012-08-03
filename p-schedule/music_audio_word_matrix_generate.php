c0e83a6f8398ac948069d95a818c8a35df9ab9cd
Fukuball Lin <fukuball@gmail.com>
2012-08-03 17:12:50 +0800

    debugging

--- p-schedule/music_audio_word_matrix_generate.php
+++ p-schedule/music_audio_word_matrix_generate.php
@@ -30,51 +30,132 @@ $query_result = $db_obj->selectCommand($select_sql);
 
 $music_feature_god = new LMMusicFeatureGod();
 
-$pitch_matrix = '';
-$timbre_matrix = '';
-
 // get unprocess data
 foreach ($query_result as $query_result_data) {
-
-   echo "song_id :".$query_result_data['id']." \n";
 
    $song_obj = new LMSong($query_result_data['id']);
    $echonest_analysis_file = AUDIO_ROOT.'/'.$song_obj->getId().'.json';
    $echonest_analysis = file_get_contents($echonest_analysis_file);
    $echonest_data = json_decode($echonest_analysis);
 
+   foreach ($echonest_data->sections as $section_data){
+      echo "section start: ".$section_data->start." \n";
+   }
+
+   /*$segment_duration_sum = 0;
+   $pitch_d1  = 0;
+   $pitch_d2  = 0;
+   $pitch_d3  = 0;
+   $pitch_d4  = 0;
+   $pitch_d5  = 0;
+   $pitch_d6  = 0;
+   $pitch_d7  = 0;
+   $pitch_d8  = 0;
+   $pitch_d9  = 0;
+   $pitch_d10 = 0;
+   $pitch_d11 = 0;
+   $pitch_d12 = 0;
+
+   $timbre_d1  = 0;
+   $timbre_d2  = 0;
+   $timbre_d3  = 0;
+   $timbre_d4  = 0;
+   $timbre_d5  = 0;
+   $timbre_d6  = 0;
+   $timbre_d7  = 0;
+   $timbre_d8  = 0;
+   $timbre_d9  = 0;
+   $timbre_d10 = 0;
+   $timbre_d11 = 0;
+   $timbre_d12 = 0;
 
    foreach ($echonest_data->segments as $segments_data) {
+      $segment_duration_sum = $segment_duration_sum+$segments_data->duration;
 
-      $pitch_vector = '';
       $count_pitch_d = 1;
       foreach ($segments_data->pitches as $pitches_data) {
 
          switch ($count_pitch_d) {
-         default:
-            $pitch_vector = $pitch_vector.$pitches_data.' ';
+         case '1':
+            $pitch_d1 = $pitch_d1+$pitches_data;
+            break;
+         case '2':
+            $pitch_d2 = $pitch_d2+$pitches_data;
+            break;
+         case '3':
+            $pitch_d3 = $pitch_d3+$pitches_data;
+         break;
+         case '4':
+            $pitch_d4 = $pitch_d4+$pitches_data;
+            break;
+         case '5':
+            $pitch_d5 = $pitch_d5+$pitches_data;
+            break;
+         case '6':
+            $pitch_d6 = $pitch_d6+$pitches_data;
+            break;
+         case '7':
+            $pitch_d7 = $pitch_d7+$pitches_data;
+            break;
+         case '8':
+            $pitch_d8 = $pitch_d8+$pitches_data;
+            break;
+         case '9':
+            $pitch_d9 = $pitch_d9+$pitches_data;
+            break;
+         case '10':
+            $pitch_d10 = $pitch_d10+$pitches_data;
+            break;
+         case '11':
+            $pitch_d11 = $pitch_d11+$pitches_data;
             break;
          case '12':
-            $pitch_vector = $pitch_vector.$pitches_data.'; ';
+            $pitch_d12 = $pitch_d12+$pitches_data;
             break;
-         }// end switch
+         }// end switch ($count_pitch_d)
 
          $count_pitch_d++;
-
       }// end foreach ($segments_data->pitches as $pitches_data)
 
-      $pitch_matrix = $pitch_matrix.$pitch_vector;
-
-      $timbre_vector = '';
       $count_timbre_d = 1;
       foreach ($segments_data->timbre as $timbre_data) {
 
          switch ($count_timbre_d) {
-         default:
-            $timbre_vector = $timbre_vector.$pitches_data.' ';
+         case '1':
+            $timbre_d1 = $timbre_d1+$timbre_data;
+            break;
+         case '2':
+            $timbre_d2 = $timbre_d2+$timbre_data;
+            break;
+         case '3':
+            $timbre_d3 = $timbre_d3+$timbre_data;
+         break;
+         case '4':
+            $timbre_d4 = $timbre_d4+$timbre_data;
+            break;
+         case '5':
+            $timbre_d5 = $timbre_d5+$timbre_data;
+            break;
+         case '6':
+            $timbre_d6 = $timbre_d6+$timbre_data;
+            break;
+         case '7':
+            $timbre_d7 = $timbre_d7+$timbre_data;
+            break;
+         case '8':
+            $timbre_d8 = $timbre_d8+$timbre_data;
+            break;
+         case '9':
+            $timbre_d9 = $timbre_d9+$timbre_data;
+            break;
+         case '10':
+            $timbre_d10 = $timbre_d10+$timbre_data;
+            break;
+         case '11':
+            $timbre_d11 = $timbre_d11+$timbre_data;
             break;
          case '12':
-            $timbre_vector = $timbre_vector.$pitches_data.'; ';
+            $timbre_d12 = $timbre_d12+$timbre_data;
             break;
          }// end switch ($count_timbre_d)
 
@@ -83,45 +164,36 @@ foreach ($query_result as $query_result_data) {
 
    }// end foreach ($echonest_data->segments as $segments_data)
 
-   $timbre_matrix = $timbre_matrix.$timbre_vector;
+   $pitch_a1  = ($pitch_d1/$segment_count);
+   $pitch_a2  = ($pitch_d2/$segment_count);
+   $pitch_a3  = ($pitch_d3/$segment_count);
+   $pitch_a4  = ($pitch_d4/$segment_count);
+   $pitch_a5  = ($pitch_d5/$segment_count);
+   $pitch_a6  = ($pitch_d6/$segment_count);
+   $pitch_a7  = ($pitch_d7/$segment_count);
+   $pitch_a8  = ($pitch_d8/$segment_count);
+   $pitch_a9  = ($pitch_d9/$segment_count);
+   $pitch_a10 = ($pitch_d10/$segment_count);
+   $pitch_a11 = ($pitch_d11/$segment_count);
+   $pitch_a12 = ($pitch_d12/$segment_count);
+
+   $timbre_a1  = ($timbre_d1/$segment_count);
+   $timbre_a2  = ($timbre_d2/$segment_count);
+   $timbre_a3  = ($timbre_d3/$segment_count);
+   $timbre_a4  = ($timbre_d4/$segment_count);
+   $timbre_a5  = ($timbre_d5/$segment_count);
+   $timbre_a6  = ($timbre_d6/$segment_count);
+   $timbre_a7  = ($timbre_d7/$segment_count);
+   $timbre_a8  = ($timbre_d8/$segment_count);
+   $timbre_a9  = ($timbre_d9/$segment_count);
+   $timbre_a10 = ($timbre_d10/$segment_count);
+   $timbre_a11 = ($timbre_d11/$segment_count);
+   $timbre_a12 = ($timbre_d12/$segment_count);*/
 
    unset($song_obj);
 }
 
 unset($music_feature_god);
-
-
-$pitch_matrix = substr ($pitch_matrix, 0, -2);
-$timbre_matrix = substr ($timbre_matrix, 0, -2);
-
-echo $pitch_matrix;
-
-/*$music_audio_word_matrix_god = new LMMusicAudioWordMatrix();
-
-$parameter_array = array();
-$parameter_array['matrix']
-    = $pitch_matrix;
-$parameter_array['type']
-    = 'pitch';
-
-if ($music_audio_word_matrix_god->create($parameter_array)) {
-   echo "create pitch audio word matrix success \n";
-} else {
-   echo "create pitch audio word matrix fail \n";
-}
-
-$parameter_array = array();
-$parameter_array['matrix']
-    = $timbre_matrix;
-$parameter_array['type']
-    = 'timbre';
-
-if ($music_audio_word_matrix_god->create($parameter_array)) {
-   echo "create timbre audio word matrix success \n";
-} else {
-   echo "create timbre audio word matrix fail \n";
-}*/
-
 
 require_once SITE_ROOT."/p-config/application-unsetter.php";
 
