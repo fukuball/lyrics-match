7448f9df4c2ca77cf6659f8bfbdcebd9ae346822
Fukuball Lin <fukuball@gmail.com>
2012-08-03 18:07:50 +0800

    debugging

--- p-schedule/music_audio_word_matrix_generate.php
+++ p-schedule/music_audio_word_matrix_generate.php
@@ -47,34 +47,6 @@ foreach ($query_result as $query_result_data) {
    $song_audio_word_array = array();
    $song_audio_word_count_array = array();
 
-   $segment_count = count($echonest_data->segments);
-
-   $pitch_d1  = 0;
-   $pitch_d2  = 0;
-   $pitch_d3  = 0;
-   $pitch_d4  = 0;
-   $pitch_d5  = 0;
-   $pitch_d6  = 0;
-   $pitch_d7  = 0;
-   $pitch_d8  = 0;
-   $pitch_d9  = 0;
-   $pitch_d10 = 0;
-   $pitch_d11 = 0;
-   $pitch_d12 = 0;
-
-   $timbre_d1  = 0;
-   $timbre_d2  = 0;
-   $timbre_d3  = 0;
-   $timbre_d4  = 0;
-   $timbre_d5  = 0;
-   $timbre_d6  = 0;
-   $timbre_d7  = 0;
-   $timbre_d8  = 0;
-   $timbre_d9  = 0;
-   $timbre_d10 = 0;
-   $timbre_d11 = 0;
-   $timbre_d12 = 0;
-
    foreach ($echonest_data->segments as $segments_data) {
 
       $pitch_start = $segments_data->start;
@@ -105,96 +77,22 @@ foreach ($query_result as $query_result_data) {
 
       }
 
+      print_r($song_audio_word_array);
 
-      $count_pitch_d = 1;
-      foreach ($segments_data->pitches as $pitches_data) {
-
-         switch ($count_pitch_d) {
-         case '1':
-            $pitch_d1 = $pitch_d1+$pitches_data;
-            break;
-         case '2':
-            $pitch_d2 = $pitch_d2+$pitches_data;
-            break;
-         case '3':
-            $pitch_d3 = $pitch_d3+$pitches_data;
-         break;
-         case '4':
-            $pitch_d4 = $pitch_d4+$pitches_data;
-            break;
-         case '5':
-            $pitch_d5 = $pitch_d5+$pitches_data;
-            break;
-         case '6':
-            $pitch_d6 = $pitch_d6+$pitches_data;
-            break;
-         case '7':
-            $pitch_d7 = $pitch_d7+$pitches_data;
-            break;
-         case '8':
-            $pitch_d8 = $pitch_d8+$pitches_data;
-            break;
-         case '9':
-            $pitch_d9 = $pitch_d9+$pitches_data;
-            break;
-         case '10':
-            $pitch_d10 = $pitch_d10+$pitches_data;
-            break;
-         case '11':
-            $pitch_d11 = $pitch_d11+$pitches_data;
-            break;
-         case '12':
-            $pitch_d12 = $pitch_d12+$pitches_data;
-            break;
-         }// end switch ($count_pitch_d)
-
-         $count_pitch_d++;
-      }// end foreach ($segments_data->pitches as $pitches_data)
-
-      $count_timbre_d = 1;
-      foreach ($segments_data->timbre as $timbre_data) {
-
-         switch ($count_timbre_d) {
-         case '1':
-            $timbre_d1 = $timbre_d1+$timbre_data;
-            break;
-         case '2':
-            $timbre_d2 = $timbre_d2+$timbre_data;
-            break;
-         case '3':
-            $timbre_d3 = $timbre_d3+$timbre_data;
-         break;
-         case '4':
-            $timbre_d4 = $timbre_d4+$timbre_data;
-            break;
-         case '5':
-            $timbre_d5 = $timbre_d5+$timbre_data;
-            break;
-         case '6':
-            $timbre_d6 = $timbre_d6+$timbre_data;
-            break;
-         case '7':
-            $timbre_d7 = $timbre_d7+$timbre_data;
-            break;
-         case '8':
-            $timbre_d8 = $timbre_d8+$timbre_data;
-            break;
-         case '9':
-            $timbre_d9 = $timbre_d9+$timbre_data;
-            break;
-         case '10':
-            $timbre_d10 = $timbre_d10+$timbre_data;
-            break;
-         case '11':
-            $timbre_d11 = $timbre_d11+$timbre_data;
-            break;
-         case '12':
-            $timbre_d12 = $timbre_d12+$timbre_data;
-            break;
-         }// end switch ($count_timbre_d)
-
-         $count_timbre_d++;
-      }// end foreach ($segments_data->pitches as $pitches_data)
+      foreach ($song_audio_word_array as $key=>$song_audio_word_dimenstion_array) {
+         $song_audio_word_array[$key][0] = $song_audio_word_dimenstion_array[0]/$song_audio_word_count_array[$key];
+         $song_audio_word_array[$key][1] = $song_audio_word_dimenstion_array[1]/$song_audio_word_count_array[$key];
+         $song_audio_word_array[$key][2] = $song_audio_word_dimenstion_array[2]/$song_audio_word_count_array[$key];
+         $song_audio_word_array[$key][3] = $song_audio_word_dimenstion_array[3]/$song_audio_word_count_array[$key];
+         $song_audio_word_array[$key][4] = $song_audio_word_dimenstion_array[4]/$song_audio_word_count_array[$key];
+         $song_audio_word_array[$key][5] = $song_audio_word_dimenstion_array[5]/$song_audio_word_count_array[$key];
+         $song_audio_word_array[$key][6] = $song_audio_word_dimenstion_array[6]/$song_audio_word_count_array[$key];
+         $song_audio_word_array[$key][7] = $song_audio_word_dimenstion_array[7]/$song_audio_word_count_array[$key];
+         $song_audio_word_array[$key][8] = $song_audio_word_dimenstion_array[8]/$song_audio_word_count_array[$key];
+         $song_audio_word_array[$key][9] = $song_audio_word_dimenstion_array[9]/$song_audio_word_count_array[$key];
+         $song_audio_word_array[$key][10] = $song_audio_word_dimenstion_array[10]/$song_audio_word_count_array[$key];
+         $song_audio_word_array[$key][11] = $song_audio_word_dimenstion_array[11]/$song_audio_word_count_array[$key];
+      }
 
       print_r($song_audio_word_array);
       print_r($song_audio_word_count_array);
