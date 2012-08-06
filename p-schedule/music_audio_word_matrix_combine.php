f2532bb0a14346f31a0f883dbca5a8e836d1e7f7
Fukuball Lin <fukuball@gmail.com>
2012-08-06 18:56:11 +0800

    audio word

--- p-schedule/music_audio_word_matrix_combine.php
+++ p-schedule/music_audio_word_matrix_combine.php
@@ -25,19 +25,29 @@ $select_sql = "SELECT ".
 $query_result = $db_obj->selectCommand($select_sql);
 
 // get unprocess data
-$audio_word_array = array();
+$p_audio_word_array = array();
+$t_audio_word_array = array();
 
 foreach ($query_result as $query_result_data) {
 
    $pitch_audio_word = $query_result_data['pitch_audio_word'];
    $pitch_audio_word_array = json_decode($pitch_audio_word);
    foreach ($pitch_audio_word_array as $key=>$value) {
-      array_push($audio_word_array, $value);
+      array_push($p_audio_word_array, $value);
+   }
+
+   $timbre_audio_word = $query_result_data['timbre_audio_word'];
+   $timbre_audio_word_array = json_decode($timbre_audio_word);
+   foreach ($timbre_audio_word_array as $key=>$value) {
+      array_push($t_audio_word_array, $value);
    }
 
 }
 
-print_r($audio_word_array);
+$p_audio_word = json_encode($p_audio_word_array);
+$t_audio_word = json_encode($t_audio_word_array);
+
+echo $t_audio_word;
 
 require_once SITE_ROOT."/p-config/application-unsetter.php";
 
