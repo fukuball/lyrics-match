93a1aefc19b860d8e807b4d5a73410c8039484bc
Fukuball Lin <fukuball@gmail.com>
2012-08-06 20:24:59 +0800

    audio word

---
+++ p-library/model/music_feature/audio_word_kmeans.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python26
+# -*- coding:utf-8 -*-
+#
+# audio_word_kmeans.py
+#
+# Python version 2.6.8
+#
+# @category Python
+# @package  /p-library/model/
+# @author   Fukuball Lin <fukuball@gmail.com>
+# @license  No Licence
+# @version  Release: <1.0>
+# @link     http://sarasti.cs.nccu.edu.tw
+#
+# hard code
+
+import sys
+import numpy as np
+import MySQLdb as mysql
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+sys.path.append("/var/www/html/lyrics-match/p-library/model")
+import ImportPath
+ImportPath.Import()
+
+import db_stage
+CONST = db_stage._Const()
+
+# connect to db
+db = mysql.connect(host    = CONST.DBHOST,
+                   user    = CONST.DBUSER,
+                   passwd  = CONST.DBPASS,
+                   db      = CONST.DBNAME)
+
+matrix_id = sys.argv[1];
+
+cur = db.cursor()
+
+cur.execute("""SELECT * FROM music_audio_word_matrix WHERE id=%s""", (matrix_id))
+
+matrix = ""
+matrix_type = ""
+
+
+for row in cur.fetchall() :
+   matrix = row[1]
+   matrix_type = row[2]
+
+print matrix
+print matrix_type
