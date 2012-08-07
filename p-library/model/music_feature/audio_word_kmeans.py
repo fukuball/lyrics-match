19c47edfef87e0946d8deccd9fd8013d76e3f9b6
Fukuball Lin <fukuball@gmail.com>
2012-08-07 16:04:39 +0800

    debugging

--- p-library/model/music_feature/audio_word_kmeans.py
+++ p-library/model/music_feature/audio_word_kmeans.py
@@ -54,7 +54,7 @@ matrix_array = json.loads(matrix)
 
 audio_word_array = np.array(matrix_array)
 #print audio_word_array.shape
-res, idx = kmeans2(audio_word_array, 80)
+res, idx = kmeans2(audio_word_array, 50)
 
 for code_word_id in idx :
    print code_word_id+1
