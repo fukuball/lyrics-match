69bae6728dcb24c8e117e93a5cda0c4d33aeddff
Fukuball Lin <fukuball@gmail.com>
2012-08-07 18:03:14 +0800

    add audio word

---
+++ p-library/model/music_feature/generate_music_audio_feature_model.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python26
+# -*- coding:utf-8 -*-
+#
+# generate_music_feature_model.py to generate music feature model
+#
+# Python version 2.6.8
+#
+# @category Python
+# @package  /p-library/model/
+# @author   Fukuball Lin <fukuball@gmail.com>
+# @license  No Licence
+# @version  Release: <1.0>
+# @link     http://sarasti.cs.nccu.edu.tw
+#
+# hard code
+
+import sys
+import numpy as np
+import MySQLdb as mysql
+import json
+np.set_printoptions(threshold=np.nan)
+
+sys.path.append("/var/www/html/lyrics-match/p-library/model")
+import ImportPath
+ImportPath.Import()
+
+import db_stage
+CONST = db_stage._Const()
+
+# connect to db
+db = mysql.connect(host    = CONST.DBHOST,
+                   user    = CONST.DBUSER,
+                   passwd  = CONST.DBPASS,
+                   db      = CONST.DBNAME)
+
+# 從資料庫抓資料
+cur = db.cursor()
+cur.execute("SELECT * FROM music_feature_matrix WHERE id=1")
+
+music_feature_matrix = "";
+row_song_id = "";
+column_music_feature = "";
+augment_music_feature = "";
+augment_matrix = "";
+create_time = "";
+modify_time = "";
+delete_time = "";
+
+for row in cur.fetchall() :
+   music_feature_matrix = row[1];
+   row_song_id = row[2];
+   column_music_feature = row[3];
+   augment_music_feature = row[4];
+   augment_matrix = row[5];
+   create_time = row[7];
+   modify_time = row[8];
+   delete_time = row[9];
+
+# music feature matrix
+A_music_feature_matrix = np.matrix(music_feature_matrix)
+#print A_music_feature_matrix
+#print( "matrix shape --> %d rows x %d columns" % A_music_feature_matrix.shape )
+
+# SVD decomposition
+music_feature_U,music_feature_s,music_feature_V = np.linalg.svd(A_music_feature_matrix, full_matrices=False)
+
+# 降維
+for s_index, s_item in enumerate(music_feature_s) :
+   if (s_item<1000) :
+      music_feature_s[s_index] = 0.0
+
+# music feature model matrix
+music_feature_s = np.diag(music_feature_s)
+#print music_feature_U
+#print( "music_feature_U shape --> %d rows x %d columns" % music_feature_U.shape )
+#print music_feature_s
+#print( "music_feature_s shape --> %d rows x %d columns" % music_feature_s.shape )
+#print music_feature_V
+#print( "music_feature_V shape --> %d rows x %d columns" % music_feature_V.shape )
+
+A_bar_music_feature_matrix = np.dot(music_feature_U,np.dot(music_feature_s,music_feature_V))
+#print A_bar_music_feature_matrix
+#print( "matrix shape --> %d rows x %d columns" % A_bar_music_feature_matrix.shape )
+
+#music_feature_matrix_subtract = np.subtract(A_music_feature_matrix, A_bar_music_feature_matrix)
+#print np.extract(music_feature_matrix_subtract>10, music_feature_matrix_subtract)
+
+A_bar_list = A_bar_music_feature_matrix.tolist()
+A_bar_string = json.dumps(A_bar_list)
+
+try:
+   cur.execute("""INSERT INTO music_feature_matrix (matrix,row_song_id,column_music_feature,augment_music_feature,augment_matrix,type,create_time,modify_time,delete_time) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""",(A_bar_string, row_song_id, column_music_feature, augment_music_feature, augment_matrix, "model", create_time, modify_time, delete_time))
+   db.commit()
+   print "success"
+except mysql.Error, e:
+   db.rollback()
+   print "An error has been passed. %s" %e
+
+
+
+cur.close()
+db.close()
---
+++ p-schedule/music_audio_feature_matrix_generate.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * music_feature_matrix_generate.php to generate matrix
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /p-schedule/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  No Licence
+ * @version  Release: <1.0>
+ * @link     http://sarasti.cs.nccu.edu.tw
+ */
+
+require_once dirname(dirname(__FILE__))."/p-config/application-setter.php";
+
+
+$db_obj = LMDBAccess::getInstance();
+
+$select_sql = "SELECT ".
+              "mf.song_id, ".
+              "mf.bar_count, ".
+              "mf.beat_count, ".
+              "mf.tatum_count, ".
+              "mf.section_count, ".
+              "mf.segment_count, ".
+              "mf.bar_avg_second, ".
+              "mf.beat_avg_second, ".
+              "mf.tatum_avg_second, ".
+              "mf.section_avg_second, ".
+              "mf.segment_avg_second, ".
+              "mf.pitch_avg_vector, ".
+              "mf.timbre_avg_vector, ".
+              "mf.pitch_std_vector, ".
+              "mf.timbre_std_vector, ".
+              "s.mode, ".
+              "s.tempo, ".
+              "s.time_signature, ".
+              "s.energy, ".
+              "s.danceability, ".
+              "s.speechiness, ".
+              "s.loudness ".
+              "FROM music_feature mf ".
+              "INNER JOIN song s ON (mf.song_id = s.id) ".
+              "WHERE mf.is_deleted = '0' ".
+              "AND s.is_deleted ='0' ".
+              "ORDER BY mf.id";
+
+$query_result = $db_obj->selectCommand($select_sql);
+
+$song_id = "";
+$music_feature = "mode,energy,loudness,ratio_of_beat_count_to_bar_count,ratio_of_tatum_count_to_beat_count,ratio_of_tatum_count_to_bar_count,tempo,danceability,bars_per_second,beat_per_second,tatums_per_second,ratio_of_avg_beat_length_to_avg_bar_length,ratio_of_avg_tatum_length_to_avg_beat_length,ratio_of_avg_tatum_length_to_avg_bar_length";
+$matirx = "";
+$augment_music_feature = "pitch_audio_word_histogram,".
+                         "timbre_audio_word_histogram";
+$augment_matrix = "";
+
+foreach ($query_result as $query_result_data) {
+
+   $song_id =  $song_id.
+               $query_result_data['song_id'].",";
+
+   if ($query_result_data['mode']==1) {
+      $mode = 10;
+   } else {
+      $mode = -10;
+   }
+
+   $matirx =   $matirx.
+               $mode." ".
+               $query_result_data['energy']." ".
+               $query_result_data['loudness']." ".
+               ($query_result_data['beat_count']/$query_result_data['bar_count'])." ".
+               ($query_result_data['tatum_count']/$query_result_data['beat_count'])." ".
+               ($query_result_data['tatum_count']/$query_result_data['bar_count'])." ".
+               $query_result_data['tempo']." ".
+               $query_result_data['danceability']." ".
+               (1/$query_result_data['bar_avg_second'])." ".
+               (1/$query_result_data['beat_avg_second'])." ".
+               (1/$query_result_data['tatum_avg_second'])." ".
+               ($query_result_data['beat_avg_second']/$query_result_data['bar_avg_second'])." ".
+               ($query_result_data['tatum_avg_second']/$query_result_data['beat_avg_second'])." ".
+               ($query_result_data['tatum_avg_second']/$query_result_data['bar_avg_second'])." ".
+               $query_result_data['speechiness'].
+               "; ";
+
+   $augment_matrix = $augment_matrix.
+                     str_replace(",", " ", $query_result_data['pitch_audio_word_histogram'])." ".
+                     str_replace(",", " ", $query_result_data['timbre_audio_word_histogram']).
+                     "; ";
+
+}
+
+$song_id = substr ($song_id, 0, -1);
+$matrix = substr ($matirx, 0, -2);
+$augment_matrix = substr ($augment_matrix, 0, -2);
+
+$music_feature_matrix_god = new LMMusicFeatureMatrixGod();
+
+$parameter_array = array();
+$parameter_array['matrix']
+    = $matrix;
+$parameter_array['row_song_id']
+    = $song_id;
+$parameter_array['column_music_feature']
+    = $music_feature;
+$parameter_array['type']
+    = 'matrix';
+$parameter_array['augment_matrix']
+    = $augment_matrix;
+$parameter_array['augment_music_feature']
+    = $augment_music_feature;
+
+if ($music_feature_matrix_god->create($parameter_array)) {
+   echo "create music feature matrix success \n";
+} else {
+   echo "create music feature matrix fail \n";
+}
+
+unset($music_feature_matrix_god);
+
+require_once SITE_ROOT."/p-config/application-unsetter.php";
+
+?>
