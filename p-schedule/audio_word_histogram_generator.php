01fb2f03e5e78ba4b3cdd69a279240ac72a9a5d5
Fukuball Lin <fukuball@gmail.com>
2012-08-07 16:55:53 +0800

    test

---
+++ p-schedule/audio_word_histogram_generator.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * echonest_vector_counter.php to count feature vector
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /p-schedule/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  No Licence
+ * @version  Release: <1.0>
+ * @link     http://sarasti.cs.nccu.edu.tw
+ */
+
+require_once dirname(dirname(__FILE__))."/p-config/application-setter.php";
+
+
+$db_obj = LMDBAccess::getInstance();
+
+$select_sql = "SELECT ".
+              "id ".
+              "FROM song ".
+              "WHERE is_deleted = '0' ".
+              "AND audio_path!='' ".
+              "AND echonest_track_id!='' ".
+              "AND retrieval_status='success' ".
+              "ORDER BY id";
+
+$query_result = $db_obj->selectCommand($select_sql);
+
+$music_feature_god = new LMMusicFeatureGod();
+
+// get unprocess data
+foreach ($query_result as $query_result_data) {
+
+
+}
+
+unset($music_feature_god);
+
+require_once SITE_ROOT."/p-config/application-unsetter.php";
+
+?>
