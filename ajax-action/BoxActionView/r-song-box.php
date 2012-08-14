4306ac602de63db3566959188e7794a8f35f22ba
Fukuball Lin <fukuball@gmail.com>
2012-08-14 20:25:16 +0800

    update

--- ajax-action/BoxActionView/r-song-box.php
+++ ajax-action/BoxActionView/r-song-box.php
@@ -15,7 +15,7 @@
 $url = "http://sarasti.cs.nccu.edu.tw/lyrics-match/p-data/mp3/1.mp3";
 
 $short_url_json = json_decode(shell_exec("curl https://www.googleapis.com/urlshortener/v1/url -H 'Content-Type: application/json' -d "."'{".'"longUrl"'.": ".'"http://www.indievox.com/"'."}'"));
-echo $short_url_json['id'];
+print_r($short_url_json);
 $message = "我發現「".$song_r_obj->title."」歌詞可以配唱「".$song_o_obj->title."」歌曲，你覺得好聽嗎？ 連結：".$url;
 $message = "連結：".$url;
 ?>
