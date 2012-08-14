3c158e853fbc1dc6f298780698464353134c8666
Fukuball Lin <fukuball@gmail.com>
2012-08-14 17:15:52 +0800

    update

---
+++ p-play-ground/fukuball/testsms.php
@@ -0,0 +1,18 @@
+<?php
+
+//variable configuration
+$host       = "hiapi.ext.hipaas.hinet.net";
+$serviceid  = "14";
+$isvid      = "a93ac44e86924497892b674d619c29fe";
+$isvkey     = "FngenvssyLCMw+A1W8TeNg==";
+$phone      = "0919095468";
+$msg        = "testtest";
+
+//get the token and sign
+$a       = LMHelper::hiapi_get_auth($host);
+$token   = $a[0];
+$sign    = $a[1];
+
+smstest();
+
+?>
