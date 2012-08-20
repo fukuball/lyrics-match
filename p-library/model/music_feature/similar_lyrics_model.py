826f612ac428173bcefd715cd177504868d98c8f
Fukuball Lin <fukuball@gmail.com>
2012-08-20 23:37:37 +0800

    debugging

--- p-library/model/music_feature/similar_lyrics_model.py
+++ p-library/model/music_feature/similar_lyrics_model.py
@@ -78,7 +78,7 @@ if (has_model_data=="true") :
    for row in cur.fetchall() :
 
       song_id = row[0]
-      print "song_id: "+song_id
+      #print "song_id: "+song_id
 
       #input_song_feature_key = song_id_array.index(song_id)
       #input_song_matrix = similar_music_model.getA()[input_song_feature_key]
