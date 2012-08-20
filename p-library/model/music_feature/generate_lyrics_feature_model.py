a3c861470497e9271e19fdf7856e40e66b2d5ca3
Fukuball Lin <fukuball@gmail.com>
2012-08-20 19:05:08 +0800

    debugging

--- p-library/model/music_feature/generate_lyrics_feature_model.py
+++ p-library/model/music_feature/generate_lyrics_feature_model.py
@@ -63,12 +63,15 @@ print "matrix formed"
 # SVD decomposition
 lyrics_feature_U,lyrics_feature_s,lyrics_feature_V = np.linalg.svd(A_lyrics_feature_matrix, full_matrices=False)
 
+print "SVD performed"
 #print lyrics_feature_s
 
 # 降維
 for s_index, s_item in enumerate(lyrics_feature_s) :
    if (s_item<1000) :
       lyrics_feature_s[s_index] = 0.0
+
+print "dedimenstion performed"
 
 # lyrics feature model matrix
 lyrics_feature_s = np.diag(lyrics_feature_s)
@@ -77,18 +80,22 @@ A_bar_lyrics_feature_matrix = np.dot(lyrics_feature_U,np.dot(lyrics_feature_s,ly
 #print A_bar_lyrics_feature_matrix
 #print( "matrix shape --> %d rows x %d columns" % A_bar_lyrics_feature_matrix.shape )
 
+print "reform matrix"
+
 A_bar_list = A_bar_lyrics_feature_matrix.tolist()
 A_bar_string = json.dumps(A_bar_list)
 
+print "matrix dump"
+
 try:
-   cur.execute("""INSERT INTO lyrics_feature_matrix (matrix,row_song_id,column_lyrics_feature,type,create_time,modify_time,delete_time) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""",(A_bar_string, row_song_id, column_lyrics_feature, "model", create_time, modify_time, delete_time))
+   cur.execute("""INSERT INTO lyrics_feature_matrix (matrix, row_song_id, column_lyrics_feature, type, create_time, modify_time, delete_time) VALUES (%s, %s, %s, %s, %s, %s, %s)""",(A_bar_string, row_song_id, column_lyrics_feature, "model", create_time, modify_time, delete_time))
    db.commit()
    print "success"
 except mysql.Error, e:
    db.rollback()
    print "An error has been passed. %s" %e
 
-
+print "save in db"
 
 cur.close()
 db.close()
