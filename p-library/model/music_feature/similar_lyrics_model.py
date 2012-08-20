1b33ed348dbfd83cb23ae5b58a775a3b5a53ec45
Fukuball Lin <fukuball@gmail.com>
2012-08-20 22:16:25 +0800

    update

--- p-library/model/music_feature/similar_lyrics_model.py
+++ p-library/model/music_feature/similar_lyrics_model.py
@@ -69,24 +69,30 @@ if (has_model_data=="true") :
    similar_music_model = lyrics_feature_matrix
    #print similar_music_model
    print( "matrix shape --> %d rows x %d columns" % similar_music_model.shape )
-   #
+
    normalize_min = similar_music_model.getA().min(axis=0)
    normalize_range = similar_music_model.getA().ptp(axis=0)
    similar_music_model_normalized = (similar_music_model.getA() - normalize_min) / normalize_range
 
-   input_song_feature_key = song_id_array.index(song_id)
-   input_song_matrix = similar_music_model.getA()[input_song_feature_key]
-   input_song_matrix_normalized = (input_song_matrix - normalize_min) / normalize_range
+   cur.execute("""SELECT id FROM song WHERE lyric!='' LIMIT 1""")
 
-   similar_music_dic = {}
-   for music_feature_index, music_feature_value in enumerate(similar_music_model_normalized):
-      similar_music_dic[song_id_array[music_feature_index]] = np.dot(music_feature_value, input_song_matrix_normalized)/(np.linalg.norm(music_feature_value)*np.linalg.norm(input_song_matrix_normalized))
+   for row in cur.fetchall() :
 
-   similar_music_sort_dic = list(sorted(similar_music_dic, key=similar_music_dic.__getitem__, reverse=True))
+      song_id = row[0]
 
-   similar_song_string = ""
-   for similar_song_id in similar_music_sort_dic :
-      similar_song_string += similar_song_id+":"+str(similar_music_dic[similar_song_id])+","
+      input_song_feature_key = song_id_array.index(song_id)
+      input_song_matrix = similar_music_model.getA()[input_song_feature_key]
+      input_song_matrix_normalized = (input_song_matrix - normalize_min) / normalize_range
 
-   similar_song_string = similar_song_string[:-1]
-   print similar_song_string
+      similar_music_dic = {}
+      for music_feature_index, music_feature_value in enumerate(similar_music_model_normalized):
+         similar_music_dic[song_id_array[music_feature_index]] = np.dot(music_feature_value, input_song_matrix_normalized)/(np.linalg.norm(music_feature_value)*np.linalg.norm(input_song_matrix_normalized))
+
+      similar_music_sort_dic = list(sorted(similar_music_dic, key=similar_music_dic.__getitem__, reverse=True))
+
+      similar_song_string = ""
+      for similar_song_id in similar_music_sort_dic :
+         similar_song_string += similar_song_id+":"+str(similar_music_dic[similar_song_id])+","
+
+      similar_song_string = similar_song_string[:-1]
+      print similar_song_string
