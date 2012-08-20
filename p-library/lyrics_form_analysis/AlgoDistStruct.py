dfd9e10e861549f168c8bc195ce6cc01f07f7828
Stuart Fan <18fanfan@gmail.com>
2012-08-20 11:44:51 +0800

    debugging

--- p-library/lyrics_form_analysis/AlgoDistStruct.py
+++ p-library/lyrics_form_analysis/AlgoDistStruct.py
@@ -311,6 +311,8 @@ class AlgoDistStruct(AlgoSequence):
 				#totalCost = self.__tableAccu[prev] + log(countDiff + 1, 2)
 				totalCost = self.__sigmoid((float(noteCount) / self.__seqI[nowCoor[0]]) - 1) + \
 						self.__sigmoid((phraseStart - phraseIdx)) - 1
+
+				totalCost *= (phraseStart - phraseIdx + 1)
 					
 			pathCosts.append({"prev": prev, "cost": totalCost})
 
@@ -332,6 +334,8 @@ class AlgoDistStruct(AlgoSequence):
 				#totalCost = self.__tableAccu[prev] + log(countDiff + 1, 2)
 				totalCost = self.__sigmoid((float(self.__seqJ[nowCoor[1]]) / wordCount) - 1) + \
 						self.__sigmoid((sentenceStart - sentenceIdx)) - 1
+
+				totalCost *= (sentenceStart - sentenceIdx + 1)
 					
 			pathCosts.append({"prev": prev, "cost": totalCost})
 
