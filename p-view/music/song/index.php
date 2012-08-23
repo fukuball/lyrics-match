160021d7296a072feb7ec36102c0235e28ec014f
Fukuball Lin <fukuball@gmail.com>
2012-08-23 20:48:54 +0800

    update

--- p-view/music/song/index.php
+++ p-view/music/song/index.php
@@ -119,7 +119,7 @@ if (!empty($_GET['song_id'])) {
                      categoryAxis.labelRotation = 90;
                      categoryAxis.gridPosition = "start";
                      categoryAxis.autoGridCount = false;
-                     categoryAxis.gridCount = 1;
+                     categoryAxis.gridCount = 10000;
 
                      // value
                      // in case you don't want to change default settings of value axis,
