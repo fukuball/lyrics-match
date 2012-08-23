8e419b5eb0983a2db55c3cce236a55d3d31d8715
Fukuball Lin <fukuball@gmail.com>
2012-08-23 18:40:04 +0800

    update

--- p-view/music/audio-code-book.php
+++ p-view/music/audio-code-book.php
@@ -24,6 +24,7 @@ $select_sql = "SELECT ".
 $query_result = $db_obj->selectCommand($select_sql);
 
 ?>
+<h2>Pitch Audio Word</h2>
 <div class="row">
 <?php
 foreach ($query_result as $query_result_data) {
@@ -88,9 +89,106 @@ foreach ($query_result as $query_result_data) {
             ?>
             var graph = new AmCharts.AmGraph();
             graph.title = "pitch<?=$count?>";
-            graph.labelText = "pitch<?=$count?>";
+            graph.labelText = "";
             graph.balloonText = "<?=round($audio_word_value, 4)?>";
             graph.valueField = "pitch<?=$count?>";
+            graph.type = "column";
+            graph.lineAlpha = 0;
+            graph.fillAlphas = <?=round($audio_word_value, 4)?>;
+            graph.lineColor = "#D41313";
+            chart<?=$audio_word_id;?>.addGraph(graph);
+            <?php
+            $count++;
+         }
+         ?>
+
+         // WRITE
+         chart<?=$audio_word_id;?>.write("chartdiv<?=$audio_word_id;?>");
+      });
+
+   </script>
+   <?php
+
+}
+
+$select_sql = "SELECT ".
+              "id, audio_word ".
+              "FROM muisc_audio_code_word ".
+              "WHERE is_deleted = '0' ".
+              "AND code_book_id = '2' ".
+              "ORDER BY id";
+
+$query_result = $db_obj->selectCommand($select_sql);
+
+?>
+<h2>Timbre Audio Word</h2>
+<div class="row">
+<?php
+foreach ($query_result as $query_result_data) {
+
+   $audio_word_id = $query_result_data['id'];
+   $audio_word = json_decode($query_result_data['audio_word']);
+
+   ?>
+   <div id="chartdiv<?=$audio_word_id;?>" class="pull-left" style="width: 100px; height: 300px;"></div>
+   <script type="text/javascript">
+      var chart<?=$audio_word_id;?>;
+
+      var chartData<?=$audio_word_id;?> = [{
+          code_word<?=$audio_word_id;?>: "code word<?=$audio_word_id;?>",
+          timbre1: 1,
+          timbre2: 1,
+          timbre3: 1,
+          timbre4: 1,
+          timbre5: 1,
+          timbre6: 1,
+          timbre7: 1,
+          timbre8: 1,
+          timbre9: 1,
+          timbre10: 1,
+          timbre11: 1,
+          timbre12: 1
+      }];
+
+      AmCharts.ready(function () {
+
+         // SERIAL CHART
+         chart<?=$audio_word_id;?> = new AmCharts.AmSerialChart();
+         chart<?=$audio_word_id;?>.dataProvider = chartData<?=$audio_word_id;?>;
+         chart<?=$audio_word_id;?>.categoryField = "code_word<?=$audio_word_id;?>";
+
+         // sometimes we need to set margins manually
+         // autoMargins should be set to false in order chart to use custom margin values
+         chart<?=$audio_word_id;?>.autoMargins = false;
+         chart<?=$audio_word_id;?>.marginLeft = 0;
+         chart<?=$audio_word_id;?>.marginRight = 0;
+         chart<?=$audio_word_id;?>.marginTop = 30;
+         chart<?=$audio_word_id;?>.marginBottom = 40;
+
+         // AXES
+         // category
+         var categoryAxis = chart<?=$audio_word_id;?>.categoryAxis;
+         categoryAxis.gridAlpha = 0;
+         categoryAxis.axisAlpha = 0;
+         categoryAxis.gridPosition = "start";
+
+         // value
+         var valueAxis = new AmCharts.ValueAxis();
+         valueAxis.stackType = "100%"; // this line makes the chart 100% stacked
+         valueAxis.gridAlpha = 0;
+         valueAxis.axisAlpha = 0;
+         valueAxis.labelsEnabled = true;
+         chart<?=$audio_word_id;?>.addValueAxis(valueAxis);
+
+         <?php
+         $count = 1;
+         foreach ($audio_word as $key=>$audio_word_value) {
+            ?>
+            var graph = new AmCharts.AmGraph();
+            graph.title = "timbre<?=$count?>";
+            graph.labelText = "";
+            graph.balloonText = "<?=round($audio_word_value, 4)?>";
+            graph.valueField = "timbre<?=$count?>";
             graph.type = "column";
             graph.lineAlpha = 0;
             graph.fillAlphas = <?=round($audio_word_value, 4)?>;
