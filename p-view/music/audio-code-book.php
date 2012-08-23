ad33046d3db4de14e342b81f02b3fd3c3e78cbc5
Fukuball Lin <fukuball@gmail.com>
2012-08-23 18:34:26 +0800

    update

--- p-view/music/audio-code-book.php
+++ p-view/music/audio-code-book.php
@@ -32,7 +32,7 @@ foreach ($query_result as $query_result_data) {
    $audio_word = json_decode($query_result_data['audio_word']);
 
    ?>
-   <div id="chartdiv<?=$audio_word_id;?>" class="pull-left" style="width: 400px; height: 300px;"></div>
+   <div id="chartdiv<?=$audio_word_id;?>" class="pull-left" style="width: 100px; height: 300px;"></div>
    <script type="text/javascript">
       var chart<?=$audio_word_id;?>;
 
@@ -70,15 +70,15 @@ foreach ($query_result as $query_result_data) {
          // AXES
          // category
          var categoryAxis = chart<?=$audio_word_id;?>.categoryAxis;
-         categoryAxis.gridAlpha = 1;
-         categoryAxis.axisAlpha = 1;
+         categoryAxis.gridAlpha = 0;
+         categoryAxis.axisAlpha = 0;
          categoryAxis.gridPosition = "start";
 
          // value
          var valueAxis = new AmCharts.ValueAxis();
          valueAxis.stackType = "100%"; // this line makes the chart 100% stacked
-         valueAxis.gridAlpha = 1;
-         valueAxis.axisAlpha = 1;
+         valueAxis.gridAlpha = 0;
+         valueAxis.axisAlpha = 0;
          valueAxis.labelsEnabled = true;
          chart<?=$audio_word_id;?>.addValueAxis(valueAxis);
 
@@ -88,7 +88,7 @@ foreach ($query_result as $query_result_data) {
             ?>
             var graph = new AmCharts.AmGraph();
             graph.title = "pitch<?=$count?>";
-            graph.labelText = "";
+            graph.labelText = "[[value]]";
             graph.balloonText = "<?=round($audio_word_value, 4)?>";
             graph.valueField = "pitch<?=$count?>";
             graph.type = "column";
