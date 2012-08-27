119f8d2dc3b6e86d551fb1a65c3a666b36e5f196
Fukuball Lin <fukuball@gmail.com>
2012-08-27 17:56:52 +0800

    update

--- p-schedule/word_net_parse.php
+++ p-schedule/word_net_parse.php
@@ -14,7 +14,8 @@
 
 require_once dirname(dirname(__FILE__))."/p-config/application-setter.php";
 
-$link = 'http://sarasti.cs.nccu.edu.tw/lyrics-match/p-schedule/word_net_crawler.php?word='.$argv[1];
+$word = '依然';
+$link = 'http://cwn.ling.sinica.edu.tw/_process.asp?inputword='.$word.'&radiobutton=1';
 
 $db_obj = LMDBAccess::getInstance();
 
@@ -22,7 +23,10 @@ $db_obj = LMDBAccess::getInstance();
 $yql_query = urlencode('SELECT * FROM html WHERE url="'.$link.'"');
 $wordnet_page_html = file_get_contents('http://query.yahooapis.com/v1/public/yql?q='.$yql_query.'&format=json');
 $wordnet_page_dom = json_decode($wordnet_page_html);
-print_r($wordnet_page_dom);
+
+$table = $wordnet_page_dom->query->results->body->table;
+
+print_r($table);
 
 
 ?>
