c1251b103876f4c90a4ec6e13970d608ab785595
Fukuball Lin <fukuball@gmail.com>
2012-08-27 17:07:09 +0800

    update

---
+++ p-schedule/word_net_parse.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * word_net_parse.php
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /p-schedule/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  No Licence
+ * @version  Release: <1.0>
+ * @link     http://sarasti.cs.nccu.edu.tw
+ */
+
+require_once dirname(dirname(__FILE__))."/p-config/application-setter.php";
+
+$link = 'http://sarasti.cs.nccu.edu.tw/lyrics-match/p-schedule/word_net_crawler.php?word='.$_GET['word'];
+
+$db_obj = LMDBAccess::getInstance();
+
+// get song detail
+$yql_query = urlencode('SELECT * FROM html WHERE url="'.$link.'"');
+$wordnet_page_html = file_get_contents('http://query.yahooapis.com/v1/public/yql?q='.$yql_query.'&format=json');
+$wordnet_page_dom = json_decode($wordnet_page_html);
+print_r($wordnet_page_dom);
+
+
+?>
