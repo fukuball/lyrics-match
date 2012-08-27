14d2e1b1ae8f2fa0790eadfe80a707af61578e05
Fukuball Lin <fukuball@gmail.com>
2012-08-27 18:47:04 +0800

    update

--- p-schedule/word_net_parse.php
+++ p-schedule/word_net_parse.php
@@ -59,7 +59,7 @@ foreach ($query_result as $query_result_data) {
       $wordnet_page_dom = json_decode($wordnet_page_html);
 
       $table = $wordnet_page_dom->query->results->body->table;
-      print $table;
+      print_r($table);
       $result_num = $table[0]->tr[0]->td->p->font[1]->content;
 
       if (is_numeric($result_num) && $result_num>0) {
