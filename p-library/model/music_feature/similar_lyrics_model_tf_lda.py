c6668825f39025898d327ad6b765589951a51fd5
Fukuball Lin <fukuball@gmail.com>
2012-09-17 23:35:09 +0800

    update

--- p-library/model/music_feature/similar_lyrics_model_tf_lda.py
+++ p-library/model/music_feature/similar_lyrics_model_tf_lda.py
@@ -27,8 +27,8 @@ db.commit()
 
 song_id = sys.argv[1];
 
-lda = models.LsiModel.load('20120917_tf_model.lda')
-index = similarities.MatrixSimilarity.load('20120917_tf_lda.index')
+lda = models.LsiModel.load('/var/www/html/lyrics-match/p-library/model/music_feature/20120917_tf_model.lda')
+index = similarities.MatrixSimilarity.load('/var/www/html/lyrics-match/p-library/model/music_feature/20120917_tf_lda.index')
 
 cur.execute("""SELECT ltt.*,ltu.id term_id FROM lyrics_term_tfidf ltt INNER JOIN lyrics_term_unique ltu ON (ltt.term=ltu.term) WHERE song_id=%s""", (song_id))
 
