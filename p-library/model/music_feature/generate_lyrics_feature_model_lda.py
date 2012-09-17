c93cced8c604b51d62cebc5d1f224d6c5c15c4c2
Fukuball Lin <fukuball@gmail.com>
2012-09-17 17:35:25 +0800

    update

--- p-library/model/music_feature/generate_lyrics_feature_model_lda.py
+++ p-library/model/music_feature/generate_lyrics_feature_model_lda.py
@@ -1,5 +1,24 @@
 import logging, gensim, bz2
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+sys.path.append("/var/www/html/lyrics-match/p-library/model")
+import ImportPath
+ImportPath.Import()
+
+import db_stage
+CONST = db_stage._Const()
+
+# connect to db
+db = mysql.connect(host    = CONST.DBHOST,
+                   user    = CONST.DBUSER,
+                   passwd  = CONST.DBPASS,
+                   db      = CONST.DBNAME,
+                   charset = 'UTF8')
+# 從資料庫抓資料
+cur = db.cursor()
+cur.execute("SET NAMES UTF8")
+cur.execute("SET CHARACTER_SET_CLIENT=UTF8")
+cur.execute("SET CHARACTER_SET_RESULTS=UTF8")
+db.commit()
 
 # load id->word mapping (the dictionary), one of the results of step 2 above
 id2word = gensim.corpora.Dictionary.load_from_text('20120917_lyrics_wordids.txt')
@@ -12,3 +31,24 @@ print mm
 # extract 20 LDA topics, using 1 pass and updating once every 1 chunk (10,000 documents)
 lda = gensim.models.ldamodel.LdaModel(corpus=mm, id2word=id2word, num_topics=20, update_every=0, chunksize=1000, passes=20)
 lda.print_topics(20)
+
+index = similarities.MatrixSimilarity(lda[mm])
+index.save('20120917_lda.index')
+
+cur.execute("""SELECT * FROM lyrics_term_tfidf_continue WHERE new_song_id=%s""", (1))
+
+new_song_id = ""
+tfidf = ""
+new_doc_list = list()
+
+for row in cur.fetchall() :
+   new_song_id = row[2];
+   tfidf = row[6];
+   the_tuple = (int(new_song_id), float(tfidf));
+   new_doc_list.append(the_tuple)
+
+print new_doc_list;
+
+new_doc_lda = lda[new_doc_list]
+sims = index[new_doc_lda]
+print list(enumerate(sims))
