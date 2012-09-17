6f6ad2105b1d1e7c06c780d930914e668331ac63
Fukuball Lin <fukuball@gmail.com>
2012-09-17 20:01:12 +0800

    update

--- p-library/model/music_feature/generate_lyrics_feature_model_lda.py
+++ p-library/model/music_feature/generate_lyrics_feature_model_lda.py
@@ -36,8 +36,11 @@ mm = gensim.corpora.MmCorpus('20120917_lyrics_tfidf.mm')
 #print mm
 
 # extract 20 LDA topics, using 1 pass and updating once every 1 chunk (10,000 documents)
-lda = gensim.models.ldamodel.LdaModel(corpus=mm, id2word=id2word, num_topics=20, update_every=100, chunksize=10000, passes=100)
-lda.VAR_MAXITER = 100;
+lda = gensim.models.ldamodel.LdaModel(num_topics=20)
+lda.VAR_MAXITER = 100
+lda.VAR_THRESH = 0.0001
+lda.update(mm, chunksize=1000, passes=100, update_every=1)
+
 lda.print_topics(20)
 
 corpus_lda = lda[mm]
