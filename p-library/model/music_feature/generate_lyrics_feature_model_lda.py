e5691e125c399f9ba2028e6e5c17618cae67c74c
Fukuball Lin <fukuball@gmail.com>
2012-09-17 17:48:00 +0800

    update

--- p-library/model/music_feature/generate_lyrics_feature_model_lda.py
+++ p-library/model/music_feature/generate_lyrics_feature_model_lda.py
@@ -1,5 +1,6 @@
 import logging, gensim, bz2
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+from gensim import corpora, models, similarities
 import sys
 import numpy as np
 import MySQLdb as mysql
@@ -36,10 +37,10 @@ mm = gensim.corpora.MmCorpus('20120917_lyrics_tfidf.mm')
 lda = gensim.models.ldamodel.LdaModel(corpus=mm, id2word=id2word, num_topics=20, update_every=0, chunksize=1000, passes=20)
 lda.print_topics(20)
 
-#corpus_lda = lda[mm]
-#for doc in corpus_lda: # both bow->tfidf and tfidf->lsi transformations are actually executed here, on the fly
-#   print doc
-#
+corpus_lda = lda[mm]
+for doc in corpus_lda: # both bow->tfidf and tfidf->lsi transformations are actually executed here, on the fly
+   print doc
+
 index = similarities.MatrixSimilarity(lda[mm])
 index.save('20120917_lda.index')
 
