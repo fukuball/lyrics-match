ab6d1588bc5f3da4cc3748b67fceea11e5999768
Fukuball Lin <fukuball@gmail.com>
2012-09-17 17:06:23 +0800

    update

--- p-schedule/word_tfidf_generate.php
+++ p-schedule/word_tfidf_generate.php
@@ -41,18 +41,30 @@ $fh = fopen($word_id_file, 'w') or die("can't open file");
 $string_data = "%%MatrixMarket matrix coordinate real general\n";
 $string_data = $string_data."$doc_num $feature_num $none_zero_entry_num\n";
 
-$select_sql = "SELECT ltt.term,ltt.song_id,ltt.tfidf,ltu.id term_id FROM lyrics_term_tfidf ltt INNER JOIN lyrics_term_unique ltu ON (ltt.term=ltu.term)";
+$select_sql = "SELECT ltt.*,ltu.id term_id FROM lyrics_term_tfidf ltt INNER JOIN lyrics_term_unique ltu ON (ltt.term=ltu.term)";
 
 $query_result = $db_obj->selectCommand($select_sql);
 
+$song_id = 0;
+$new_song_id = 0;
+
 foreach ($query_result as $query_result_data) {
 
-   $doc_id = $query_result_data['song_id'];
-   $term_id = ($query_result_data['term_id']-1);
+   if ($query_result_data['song_id']!=$song_id) {
+      $song_id = $query_result_data['song_id'];
+      $new_song_id++;
+   }
+   $term_id = $query_result_data['term_id'];
    $tfidf = $query_result_data['tfidf'];
+   $term = $query_result_data['term'];
+   $pos = $query_result_data['pos'];
+   $tf = $query_result_data['tf'];
 
-   $string_data = $string_data."$doc_id\t$term_id\t$tfidf\n";
-   echo "$doc_id\t$term_id\t$tfidf\n";
+   $string_data = $string_data."$new_song_id\t$term_id\t$tfidf\n";
+   echo "$new_song_id\t$term_id\t$tfidf\n";
+
+   $insert_sql = "INSERT INTO lyrics_term_tfidf_continue (song_id,new_song_id,term,pos,tf,tfidf,create_time,modify_time) VALUES ('".addslashes($song_id)."', '".addslashes($new_song_id)."', '".addslashes($term)."', '".addslashes($pos)."', '".addslashes($tf)."', '".addslashes($tfidf)."', NOW(), NOW())";
+   $query_result3 = $db_obj->insertCommand($insert_sql);
 
 }// end foreach ($query_result as $query_result_data) {
 
