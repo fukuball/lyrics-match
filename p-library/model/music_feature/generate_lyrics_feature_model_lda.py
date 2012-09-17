843967360343e8697a2d0ce42fc639c65cd8febc
Fukuball Lin <fukuball@gmail.com>
2012-09-17 17:50:59 +0800

    update

--- p-library/model/music_feature/generate_lyrics_feature_model_lda.py
+++ p-library/model/music_feature/generate_lyrics_feature_model_lda.py
@@ -58,6 +58,8 @@ for row in cur.fetchall() :
 
 print new_doc_list
 
+print "similarity..."
+
 new_doc_lda = lda[new_doc_list]
-sims = index[new_doc_lda]
-print list(enumerate(sims))
+sims = sorted(enumerate(sims), key=lambda item: -item[1])
+print sims # print sorted (document number, similarity score) 2-tuples
