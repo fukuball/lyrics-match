a5f56a9f1e27e8dacfe6efe0a93cbbc81871e95f
Fukuball Lin <fukuball@gmail.com>
2012-09-17 16:07:55 +0800

    update

---
+++ p-library/model/music_feature/generate_lyrics_feature_model_lda.py
@@ -0,0 +1,6 @@
+import logging, gensim, bz2
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+# load id->word mapping (the dictionary), one of the results of step 2 above
+id2word = gensim.corpora.Dictionary.load_from_text('20120917_lyrics_wordids.txt')
+print id2word
