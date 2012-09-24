6d514452b196ab73a290b5f3d3a9834bb1cecd87
Fukuball Lin <fukuball@gmail.com>
2012-09-24 15:14:43 +0800

    update

--- p-schedule/word_tfidf_generate_ch.php
+++ p-schedule/word_tfidf_generate_ch.php
@@ -36,10 +36,15 @@ foreach ($query_result as $query_result_data) {
 
 
 $word_id_file = "/var/www/html/lyrics-match/p-library/model/music_feature/20120924_lyrics_tfidf_ch.mm";
+$word_id_file_tf = "/var/www/html/lyrics-match/p-library/model/music_feature/20120924_lyrics_tf_ch.mm";
 $fh = fopen($word_id_file, 'w') or die("can't open file");
+$fh_tf = fopen($word_id_file_tf, 'w') or die("can't open file");
 
 $string_data = "%%MatrixMarket matrix coordinate real general\n";
 $string_data = $string_data."$doc_num $feature_num $none_zero_entry_num\n";
+
+$string_data_tf = "%%MatrixMarket matrix coordinate real general\n";
+$string_data_tf = $string_data_tf."$doc_num $feature_num $none_zero_entry_num\n";
 
 $select_sql = "SELECT ltt.*,ltu.id term_id FROM lyrics_term_tfidf_ch ltt INNER JOIN lyrics_term_unique_ch ltu ON (ltt.term=ltu.term) ORDER BY ltt.song_id";
 
@@ -63,6 +68,9 @@ foreach ($query_result as $query_result_data) {
    $string_data = $string_data."$new_song_id\t$term_id\t$tfidf\n";
    echo "$new_song_id\t$term_id\t$tfidf\n";
 
+   $string_data_tf = $string_data_tf."$new_song_id\t$term_id\t$tf\n";
+   echo "$new_song_id\t$term_id\t$tf\n";
+
    $insert_sql = "INSERT INTO lyrics_term_tfidf_continue_ch (song_id,new_song_id,term,pos,tf,tfidf,create_time,modify_time) VALUES ('".addslashes($song_id)."', '".addslashes($new_song_id)."', '".addslashes($term)."', '".addslashes($pos)."', '".addslashes($tf)."', '".addslashes($tfidf)."', NOW(), NOW())";
    $query_result3 = $db_obj->insertCommand($insert_sql);
 
@@ -70,6 +78,9 @@ foreach ($query_result as $query_result_data) {
 
 fwrite($fh, $string_data);
 fclose($fh);
+
+fwrite($fh_tf, $string_data_tf);
+fclose($fh_tf);
 
 require_once SITE_ROOT."/p-config/application-unsetter.php";
 
